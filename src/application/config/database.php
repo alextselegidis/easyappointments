4dbcdc289d783011265fb73581530cf034484900
alextselegidis@gmail.com <alextselegidis@gmail.com@9c3f3e9a-ba09-77dc-566e-c2017f8391d2>
2013-04-12 16:36:09 +0000

    Προσθήκη τεκμηρίωσης Google Calendar API και των βιβλιοθηκών Javascript και CSS στον κώδικα της εφαρμογής.

--- src/application/config/database.php
+++ src/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
+$db['default']['username'] = 'root';
 $db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['database'] = 'easy_appointments';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
