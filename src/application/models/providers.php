918903de624ec20dbb5dfae85b2e383533c2b64e
alextselegidis@gmail.com <alextselegidis@gmail.com@9c3f3e9a-ba09-77dc-566e-c2017f8391d2>
2013-04-16 21:37:36 +0000

    Υλοποίηση κύριας λειτουργίας των τεσσάρων βημάτων της κράτησης ραντεβού. Λείπουν επιπλέον λειτουργίες ελέχγου των δεδομένων και η μορφοποίηση της σελίδας.

--- src/application/models/providers.php
+++ src/application/models/providers.php
@@ -1,7 +1,44 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+class Providers extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    /**
+     * This method returns the available providers and 
+     * the services that can provide.
+     * 
+     * @return array Returns an array with the providers
+     * data.
+     */
+    public function getAvailableProviders() {
+        $sql = '
+            SELECT ea_users.* 
+            FROM ea_users 
+            INNER JOIN ea_roles 
+                ON ea_users.id_roles = ea_roles.id 
+            WHERE ea_roles.slug = "provider"';
+        
+        $providers = $this->db->query($sql)->result_array();
+        
+        foreach($providers as &$provider) {
+            $sql = '
+                SELECT id_services
+                FROM ea_services_providers
+                WHERE id_users = ' . $this->db->escape($provider['id']);
+            
+            $providerServices = $this->db->query($sql)->result_array();
+            
+            if (!isset($provider['services'])) {
+                $provider['services'] = array();
+            }
+            
+            foreach($providerServices as $providerService) {
+                $provider['services'][] = $providerService['id_services'];
+            }
+        }
+        
+        return $providers;
+    }
+}
 ?>
