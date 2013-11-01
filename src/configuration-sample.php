1feb84270deaf3dca8d845b5b6056a7eba4b35f9
alextselegidis@gmail.com <alextselegidis@gmail.com@9c3f3e9a-ba09-77dc-566e-c2017f8391d2>
2013-11-01 17:27:06 +0000

    * Added development info (read-me.txt) * Created the configuration-sample.php file and removed the configuration.php (the later must be created only on every installation).

---
+++ src/configuration-sample.php
@@ -0,0 +1,21 @@
+<?php
+class SystemConfiguration {
+    // General Settings
+    public static $base_url    = 'http://localhost/dev/external/easy_appointments/trunk/src/';
+    
+    // Database Settings
+    public static $db_host     = 'localhost';
+    public static $db_name     = 'new_ea';
+    public static $db_username = 'root';
+    public static $db_password = '';
+    
+    // Google Calendar API Settings
+    public static $google_sync_feature  = TRUE; // Enter TRUE or FALSE;
+    public static $google_product_name  = 'Easy!Appointments';
+    public static $google_client_id     = '396094740598-l9ohhdgs0hr6qi89628p3chf9lm59mkc.apps.googleusercontent.com';
+    public static $google_client_secret = '3kKEgx3mgxfFInrWf3jTUn4D';
+    public static $google_api_key       = 'AIzaSyCLMin3-ePz8xShJ7klSduo7iChDV7ldc0';
+}
+
+/* End of file configuration.php */
+/* Location: ./configuration.php */
