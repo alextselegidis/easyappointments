a192e33b440c63c9de505479a5b99b296cd74bff
Alex Tselegidis <alextselegidis@gmail.com>
2015-01-28 23:54:11 +0100

    Replaced phpdoc with apigen

--- rsc/code-doc.sh
+++ rsc/code-doc.sh
@@ -1,10 +1,8 @@
 #!bin/bash
-cd ${0%/*}
 
-# Generate PHP Documentation (requires phpdoc in /usr/bin directory).
+# Generate PHP Documentation.
 rm -rf "../doc/code/php/*"
-phpdoc -d "../src/application/controllers,../src/application/models,../src/application/libraries" -t "../doc/code/php" -i "*libraries/external/*"
-find "../doc/code/php" -name "phpdoc-cache*" -delete
+php apigen.phar generate -s "../src/application/controllers,../src/application/models,../src/application/libraries" -d "../doc/code/php" --exclude "*external*" --tree --todo --template-theme "bootstrap"
 
 # Generate JS Documentation (requires jsdoc in nodejs environment). 
 rm -rf "../doc/code/js/*"
