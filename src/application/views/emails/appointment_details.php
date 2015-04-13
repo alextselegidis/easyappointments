cb0c44e14ac8c3b802fa096bb39534d597256683
Alex Tselegidis <alextselegidis@gmail.com>
2015-04-13 20:34:25 +0200

    Minor modifications in the styling of the emails.

--- src/application/views/emails/appointment_details.php
+++ src/application/views/emails/appointment_details.php
@@ -3,9 +3,9 @@
     <title>Appointment Details</title>
 </head>
 <body style="font: 13px arial, helvetica, tahoma;">
-    <div class="email-container" style="width: 650px;border: 1px solid #eee;">
+    <div class="email-container" style="width: 650px; border: 1px solid #eee;">
         <div id="header" style="background-color: #3DD481; border-bottom: 4px solid #1A865F; 
-                height: 45px;padding: 10px 15px;">
+                height: 45px; padding: 10px 15px;">
             <strong id="logo" style="color: white; font-size: 20px; 
                     text-shadow: 1px 1px 1px #8F8888; margin-top: 10px; display: inline-block">
                     $company_name</strong>
