41136a6d66321e734fac710230f6eeae37fc8089
Alex Tselegidis <alextselegidis@gmail.com>
2015-04-26 16:32:50 +0200

    Converted message.php to responsive.

--- src/application/views/appointments/message.php
+++ src/application/views/appointments/message.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title><?php echo $company_name; ?></title>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8" /> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#35A768">
+    <title><?php echo $company_name; ?></title>
     
-    <?php // INCLUDE JS FILES ?>
+    <?php
+        // ------------------------------------------------------------
+        // INCLUDE JS FILES 
+        // ------------------------------------------------------------ ?>
     <script 
         type="text/javascript" 
         src="<?php echo $this->config->base_url(); ?>/assets/js/libs/jquery/jquery.min.js"></script>
@@ -20,53 +24,60 @@
         type="text/javascript" 
         src="<?php echo $this->config->base_url(); ?>/assets/js/general_functions.js"></script>
         
-    <?php // INCLUDE CSS FILES ?>
+    <?php
+        // ------------------------------------------------------------
+        // INCLUDE CSS FILES 
+        // ------------------------------------------------------------ ?>
     <link rel="stylesheet" type="text/css" 
         href="<?php echo $this->config->base_url(); ?>/assets/ext/bootstrap/css/bootstrap.css">
-   
+    <link rel="stylesheet" type="text/css" 
+        href="<?php echo $this->config->base_url(); ?>/assets/css/frontend.css">
     
-    <?php // SET FAVICON FOR PAGE ?>
+    <?php
+        // ------------------------------------------------------------
+        // SET PAGE FAVICON 
+        // ------------------------------------------------------------ ?>
     <link rel="icon" type="image/x-icon" 
         href="<?php echo $this->config->base_url(); ?>/assets/img/favicon.ico">
 
     <link rel="icon" sizes="192x192" 
         href="<?php echo $this->config->base_url(); ?>/assets/img/logo.png">
-    
-    <style>
-        body {
-            background-color: #CAEDF3;
-        }
-        
-        #message-frame {
-            width: 630px;
-            margin: 150px auto 0 auto;
-            background: #FFF;
-            border: 1px solid #DDDADA;
-            padding: 70px;
-        }
-        
-        #message-icon {
-            float: right;
-            margin-top: 17px;
-        }
-    </style>
 </head>
+
 <body>
-    <div id="message-frame" class="frame-container">
-        <img id="message-icon" src="<?php echo $message_icon; ?>" />
-        <h3><?php echo $message_title; ?></h3>
-        <p><?php echo $message_text; ?></p>  
-        <?php 
-            // Display exceptions (if any).
-            if (isset($exceptions)) {
-                echo '<div style="margin: 10px">';
-                echo '<h4>Unexpected Errors</h4>';
-                foreach($exceptions as $exception) {
-                    echo exceptionToHtml($exception);
-                }
-                echo '</div>';
-            }
-        ?>
+    <div id="main" class="container">
+        <div class="wrapper row">
+
+            <div id="message-frame" class="frame-container 
+                    col-xs-12 
+                    col-sm-offset-1 col-sm-10 
+                    col-md-offset-2 col-md-8 
+                    col-lg-offset-2 col-lg-8">
+
+                <div class="col-xs-12 col-sm-2">                    
+                    <img id="message-icon" src="<?php echo $message_icon; ?>" />
+                </div>
+
+                <div class="col-xs-12 col-sm-10">
+                    <?php 
+
+                        echo '<h3>' . $message_title . '</h3>';
+                        echo '<p>' . $message_text . '</p>';  
+                    
+                        // Display exceptions (if any).
+                        if (isset($exceptions)) {
+                            echo '<div style="margin: 10px">';
+                            echo '<h4>Unexpected Errors</h4>';
+                            foreach($exceptions as $exception) {
+                                echo exceptionToHtml($exception);
+                            }
+                            echo '</div>';
+                        }
+                    ?>
+                </div>
+            </div>
+        
+        </div>
     </div>
 </body>
 </html>
