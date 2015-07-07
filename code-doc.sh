256aaeb82a6c76f6bcd273beada1cef507f99974
Alex Tselegidis <alextselegidis@gmail.com>
2015-07-07 22:59:49 +0200

    Created new code-doc.sh file

--- code-doc.sh
+++ code-doc.sh
@@ -8,7 +8,11 @@ rm -rf doc
 
 mkdir doc 
 mkdir doc/apigen
+mkdir doc/jsdoc
 
 php rsc/apigen.phar generate \
     -s "src/application/controllers,src/application/models,src/application/libraries" \
     -d "doc/apigen" --exclude "*external*" --tree --todo --template-theme "bootstrap"
+
+
+#jsdoc "src/assets/js" -d "doc/jsdoc"
