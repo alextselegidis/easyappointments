450b7b3202bdf29f0abc1dd5f7285c0e1c0a8be4
Alex Tselegidis <alextselegidis@gmail.com>
2015-07-20 21:44:35 +0200

    Removed unnecessary comments.

--- rsc/db/sample-data.sql
+++ rsc/db/sample-data.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jan 14, 2015 at 10:50 PM
--- Server version: 5.5.40-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
