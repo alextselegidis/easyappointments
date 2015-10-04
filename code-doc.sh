422a7c7bfb5041c33bce27dff36cedd684009a55
Alex Tselegidis <alextselegidis@gmail.com>
2015-10-04 20:55:11 +0200

    Added plato jsmetrics to package.json

--- code-doc.sh
+++ code-doc.sh
@@ -9,6 +9,7 @@ rm -rf doc
 mkdir doc 
 mkdir doc/apigen
 mkdir doc/jsdoc
+mkdir doc/plato
 
 php rsc/apigen.phar generate \
     -s "src/application/controllers,src/application/models,src/application/libraries" \
@@ -16,3 +17,5 @@ php rsc/apigen.phar generate \
 
 
 node node_modules/.bin/jsdoc "src/assets/js" -d "doc/jsdoc"
+
+node node_modules/.bin/plato -r -d "doc/plato" "src/assets/js"
