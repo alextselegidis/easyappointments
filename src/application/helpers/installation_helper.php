416428ee4ea23dfe7ec51ccb2a6aa968928d9c30
Alex Tselegidis <alextselegidis@gmail.com>
2015-10-18 19:51:24 +0200

    Created new installation_helper.php file.

---
+++ src/application/helpers/installation_helper.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Check if Easy!Appointments is installed.
+ *
+ * This function will check some factors to determine if Easy!Appointments is
+ * installed or not. It is possible that the installation is properly configure
+ * without being recognized by this method.
+ *
+ * Notice: You can add more checks into this file in order to further check the
+ * installation state of the application.
+ *
+ * @return bool Returns whether E!A is installed or not.
+ */
+function is_ea_installed() {
+    return $this->db->table_exists('ea_users');
+
+}
