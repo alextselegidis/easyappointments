b68afb8a714fc48e09a6cd1a02959e88e0256c52
Alex Tselegidis <alextselegidis@gmail.com>
2015-10-18 19:48:39 +0200

    Added comment banners to helpers.

--- src/application/helpers/data_validation_helper.php
+++ src/application/helpers/data_validation_helper.php
@@ -1,12 +1,23 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2015, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
 
 /**
- * Check if a string date is valid for insertion or update 
- * to the database. 
- * 
+ * Check if a string date is valid for insertion or update
+ * to the database.
+ *
  * @param string $datetime The given date.
  * @return bool Returns the validation result.
- * 
+ *
  * @link http://stackoverflow.com/a/8105844/1718162 [SOURCE]
  */
 function validate_mysql_datetime($datetime) {
