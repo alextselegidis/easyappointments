95a5ae1dce2c9452c24bc00c6cf82b33ec1be3d4
Alex Tselegidis <alextselegidis@gmail.com>
2015-12-30 14:18:18 +0100

    Implemented the 'gulp composer' task which will install the composer dependencies and move only the required files to the 'src/application/third_party' directory.

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp'),
+    exec = require('child_process').execSync,
+    del = require('del');
+
+/**
+ * Install and copy the required files from the "composer" directory.
+ *
+ * Composer needs to be installed and configured in order for this command to
+ * work properly.
+ */
+gulp.task('composer', function() {
+    del.sync([
+        './composer',
+        './src/application/third_party/**/*',
+        '!./src/application/third_party/index.html',
+    ]);
+
+    exec('composer install --no-dev --prefer-dist', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+    });
+
+    return gulp.src([
+        'composer/**/*',
+        '!composer/**/demo{,/**}',
+        '!composer/**/{demo,docs,examples,test,extras}{,/**}',
+        '!composer/**/{composer.json,composer.lock,.gitignore}',
+        '!composer/**/*.yml',
+        '!composer/**/*.md'
+    ])
+        .pipe(gulp.dest('./src/application/third_party/'));
+});
+
+/**
+ * Build the project and create an easyappointments.zip file ready for distribution.
+ */
+gulp.task('build', function() {
+
+});
+
+/**
+ * Generate code documentation.
+ */
+gulp.task('doc', function() {
+
+});
