86644cfa9c0aaf47b9ef341142aa2410ceebfa6b
Alex Tselegidis <alextselegidis@gmail.com>
2016-01-02 14:47:04 +0100

    Updated the copyright date.

--- src/assets/js/frontend_book_success.js
+++ src/assets/js/frontend_book_success.js
@@ -3,7 +3,7 @@
  * 
  * @package     EasyAppointments
  * @author      A.Tselegidis <alextselegidis@gmail.com>
- * @copyright   Copyright (c) 2013 - 2015, Alex Tselegidis
+ * @copyright   Copyright (c) 2013 - 2016, Alex Tselegidis
  * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3 
  * @link        http://easyappointments.org
  * @since       v1.0.0
