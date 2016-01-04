9918631c0fed6e9c988d0ec05acac3c3b1daa4a6
Alex Tselegidis <alextselegidis@gmail.com>
2016-01-04 23:28:52 +0100

    Fixes #43 problem with warnings.

--- src/assets/sql/structure.sql
+++ src/assets/sql/structure.sql
@@ -1,17 +1,5 @@
--- ----------------------------------------------------------------------------
--- Easy!Appointments - Open Source Web Scheduler
--- 
--- @package     EasyAppointments
--- @author      A.Tselegidis <alextselegidis@gmail.com>
--- @copyright   Copyright (c) 2013 - 2016, Alex Tselegidis
--- @license     http://opensource.org/licenses/GPL-3.0 - GPLv3 
--- @link        http://easyappointments.org
--- @since       v1.0.0
--- ----------------------------------------------------------------------------
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 CREATE TABLE IF NOT EXISTS `ea_appointments` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
