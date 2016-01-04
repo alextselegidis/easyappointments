3a5227f687bc5cc6e8dcf094d6b153fd8dc8709a
Alex Tselegidis <alextselegidis@gmail.com>
2016-01-04 23:07:45 +0100

    Added copyright comments in the data.sql file.

--- src/assets/sql/data.sql
+++ src/assets/sql/data.sql
@@ -1,3 +1,14 @@
+-- ----------------------------------------------------------------------------
+-- Easy!Appointments - Open Source Web Scheduler
+--
+-- @package     EasyAppointments
+-- @author      A.Tselegidis <alextselegidis@gmail.com>
+-- @copyright   Copyright (c) 2013 - 2016, Alex Tselegidis
+-- @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+-- @link        http://easyappointments.org
+-- @since       v1.1.0
+-- ----------------------------------------------------------------------------
+
 INSERT INTO ea_settings (name, value) VALUES
     ('google_analytics_code', ''),
     ('customer_notifications', '1'),
