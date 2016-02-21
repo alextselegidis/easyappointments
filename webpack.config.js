7eab57704a2b9ee07505c5c10d2e7863353eed3a
Alex Tselegidis <alextselegidis@gmail.com>
2016-02-21 11:30:04 +0100

    Configured webpack for font-awesome integration.

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,16 @@ module.exports = {
     entry: './script.js',
     output: {
         path: __dirname,
-        filename: 'script.min.js'
+        filename: 'bundle.js'
     },
     devtool: 'inline-source-map',
     module: {
         loaders: [
-            { test: /\.css$/, loader: 'style!css!postcss' },
+            { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel?presets[]=es2015' },
-            { test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/, loader: 'url?limit=100000' }
+            { test: /\.(woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/, 
+                loader: 'url?limit=100000&name=assets/asset-[hash].[ext]' },
+            { test: /\.(eot)(\?\S*)?$/, loader: 'file?name=assets/asset-[hash].[ext]'}
         ]
     },
     postcss: function() {
