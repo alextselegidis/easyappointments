af9545ca3381a8dbb205e6832c24c77c0e98fd23
Alex Tselegidis <alextselegidis@gmail.com>
2016-02-21 14:52:13 +0100

    Implemented the landing page (bundled assets go to the assets directory).

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 'use strict';
 
-// Require Dependencies
+// Require Assets
 window.jQuery = window.$ = require('./node_modules/jquery/dist/jquery.min.js'); 
-require('./node_modules/bootstrap/dist/js/bootstrap.min.js');
 require('./node_modules/fancybox/dist/js/jquery.fancybox.js');
+require('./node_modules/bootstrap/dist/js/bootstrap.min.js');
 require('./node_modules/bootstrap/dist/css/bootstrap.min.css');
-require('./node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff');
+require('./node_modules/font-awesome/css/font-awesome.css');
+require('./node_modules/open-sans-fontface/open-sans.css');
 require('./style.css'); 
