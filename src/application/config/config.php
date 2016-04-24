528fa89400ebb389501046964646a1fe792b2843
Alex Tselegidis <alextselegidis@gmail.com>
2016-04-24 18:03:21 +0200

    Renamed session variables to the CIv3 equivalents.

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -291,15 +291,13 @@ $config['encryption_key'] = Config::BASE_URL;
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
+$config['sess_driver']           = 'files';
 $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
-$config['sess_encrypt_cookie']	= FALSE;
-$config['sess_use_database']	= FALSE;
-$config['sess_table_name']		= 'ci_sessions';
+$config['sess_save_path']       = NULL;
 $config['sess_match_ip']		= FALSE;
-$config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update']	= 300;
+$config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
