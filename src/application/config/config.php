be736e2c5cb4224f8207fca7c1b9406dd819f5a3
Alex Tselegidis <alextselegidis@gmail.com>
2016-04-24 18:06:11 +0200

    Corrected equation symbol alignment.

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -291,12 +291,12 @@ $config['encryption_key'] = Config::BASE_URL;
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_driver']           = 'files';
-$config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
-$config['sess_save_path']       = NULL;
-$config['sess_match_ip']		= FALSE;
-$config['sess_time_to_update']	= 300;
+$config['sess_driver']             = 'files';
+$config['sess_cookie_name']		   = 'ci_session';
+$config['sess_expiration']		   = 7200;
+$config['sess_save_path']          = NULL;
+$config['sess_match_ip']		   = FALSE;
+$config['sess_time_to_update']	   = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
