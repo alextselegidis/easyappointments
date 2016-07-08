ac7d8774114a395a05e875ef79d584a2ce6d50f0
Alex Tselegidis <alextselegidis@gmail.com>
2016-07-08 21:14:38 +0200

    Created parent API controller class.

---
+++ src/application/controllers/api/v1/API_V1_Controller.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2016, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.2.0
+ * ---------------------------------------------------------------------------- */
+
+class API_V1_Controller extends CI_Controller {
+
+}
+
+/* End of file API_V1_Controller.php */
+/* Location: ./application/controllers/API_V1_Controller.php */
