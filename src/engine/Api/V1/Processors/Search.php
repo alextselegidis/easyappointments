f2e1af7cb4b3a6854102af3073df2c0c067cbb51
Alex Tselegidis <alextselegidis@gmail.com>
2016-07-09 22:43:31 +0200

    Implemented search processor.

--- src/engine/Api/V1/Processors/Search.php
+++ src/engine/Api/V1/Processors/Search.php
@@ -15,6 +15,31 @@ namespace EA\Engine\Api\V1\Processors;
 
 class Search implements ProcessorsInterface {
     public static function process(array &$response) {
-        
+        if (!isset($_GET['q'])) {
+            return;
+        }
+
+        $searchedResponse = []; 
+        $keyword = (string)$_GET['q']; 
+
+        foreach ($response as $entry) {
+            if (self::_recursiveArraySearch($entry, $keyword) !== false) {
+                $searchedResponse[] = $entry;
+            }
+        }
+
+        $response = $searchedResponse;
+    }
+
+    protected static function _recursiveArraySearch(array $haystack, $needle) {
+        foreach ($haystack as $key => $value) {
+            $currentKey = $key;
+
+            if ($needle === $value || (is_array($value) && self::_recursiveArraySearch($value, $needle) !== false)) {
+                return $currentKey;
+            }
+        }
+
+        return false;
     }
 }
