0bd1da56e18439a233d928dd6fdccd1db669bef2
Alex Tselegidis <alextselegidis@gmail.com>
2016-07-10 14:17:29 +0200

    Implemented providers API resource controller and parser.

--- src/application/controllers/api/v1/Providers.php
+++ src/application/controllers/api/v1/Providers.php
@@ -13,6 +13,10 @@
 
 require_once __DIR__ . '/API_V1_Controller.php';
 
+use \EA\Engine\Api\V1\Response;
+use \EA\Engine\Api\V1\Request;
+use \EA\Engine\Types\NonEmptyString;
+
 /**
  * Providers Controller
  *
@@ -21,52 +25,124 @@ require_once __DIR__ . '/API_V1_Controller.php';
  */
 class Providers extends API_V1_Controller {
     /**
+     * Providers Resource Parser
+     * 
+     * @var \EA\Engine\Api\V1\Parsers\Providers
+     */
+    protected $parser;
+
+    /**
      * Class Constructor
      */
     public function __construct() {
         parent::__construct();
+        $this->load->model('providers_model'); 
+        $this->parser = new \EA\Engine\Api\V1\Parsers\Providers;
     }
 
     /**
      * GET API Method 
      * 
-     * @param int $id Optional (null), the record ID to be returned.
-     * 
-     * @return \EA\Engine\Api\V1\Response Returns data response. 
+     * @param int $id Optional (null), the record ID to be returned. 
      */
     public function get($id = null) {
-        
+        try {
+            $condition = $id !== null ? 'id = ' . $id : null; 
+            $providers = $this->providers_model->get_batch($condition); 
+
+            if ($id !== null && count($providers) === 0) {
+                $this->_throwRecordNotFound();
+            }
+
+            $response = new Response($providers);
+
+            $response->encode($this->parser)
+                    ->search()
+                    ->sort()
+                    ->paginate()
+                    ->minimize()
+                    ->singleEntry($id)
+                    ->output();
+
+        } catch (\Exception $exception) {
+            $this->_handleException($exception);
+        }
     }
 
     /**
      * POST API Method 
-     * 
-     * @return @return \EA\Engine\Api\V1\Response Returns data response. 
      */
     public function post() {
-        
+        try {
+            // Insert the provider to the database. 
+            $request = new Request(); 
+            $provider = $request->getBody(); 
+            $this->parser->decode($provider); 
+            
+            if (isset($provider['id'])) {
+                unset($provider['id']);
+            }
+
+            $id = $this->providers_model->add($provider);
+
+            // Fetch the new object from the database and return it to the client.
+            $batch = $this->providers_model->get_batch('id = ' . $id); 
+            $response = new Response($batch); 
+            $status = new NonEmptyString('201 Created');
+            $response->encode($this->parser)->singleEntry(true)->output($status);
+        } catch (\Exception $exception) {
+            $this->_handleException($exception);
+        }
     }
 
     /**
      * PUT API Method 
      *
-     * @param int $id The record ID to be updated.
-     * 
-     * @return @return \EA\Engine\Api\V1\Response Returns data response. 
+     * @param int $id The record ID to be updated. 
      */
     public function put($id) {
+        try {
+            // Update the provider record. 
+            $batch = $this->providers_model->get_batch('id = ' . $id); 
 
+            if ($id !== null && count($batch) === 0) {
+                $this->_throwRecordNotFound();
+            }
+            
+            $request = new Request(); 
+            $updatedProvider = $request->getBody(); 
+            $baseProvider = $batch[0];
+            $this->parser->decode($updatedProvider, $baseProvider); 
+            $updatedProvider['id'] = $id; 
+            $id = $this->providers_model->add($updatedProvider);
+            
+            // Fetch the updated object from the database and return it to the client.
+            $batch = $this->providers_model->get_batch('id = ' . $id); 
+            $response = new Response($batch); 
+            $response->encode($this->parser)->singleEntry($id)->output(); 
+        } catch (\Exception $exception) {
+            $this->_handleException($exception);
+        }
     }
 
     /**
      * DELETE API Method 
      *
-     * @param int $id The record ID to be deleted.
-     * 
-     * @return @return \EA\Engine\Api\V1\Response Returns data response. 
+     * @param int $id The record ID to be deleted. 
      */
     public function delete($id) {
+        try {
+            $result = $this->providers_model->delete($id);
 
+            $response = new Response([
+                'code' => 200, 
+                'message' => 'Record was deleted successfully!'
+            ]);
+
+            $response->output();
+        } catch (\Exception $exception) {
+            $this->_handleException($exception);
+        }
     }
 }
 
