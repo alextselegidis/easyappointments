a9edbda7a79b9c6e7c7902fc1114519f4bdc7e34
Alex Tselegidis <alextselegidis@gmail.com>
2016-07-17 13:46:38 +0200

    Split the calendar view functionality into new module (#182).

--- src/application/views/backend/calendar.php
+++ src/application/views/backend/calendar.php
@@ -37,7 +37,7 @@
     };
 
     $(document).ready(function() {
-        BackendCalendar.initialize(true);
+        BackendCalendar.initialize(GlobalVariables.calendarView);
     });
 </script>
 
---
+++ src/assets/js/backend_calendar_table_view.js
@@ -0,0 +1,32 @@
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2016, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.2.0
+ * ---------------------------------------------------------------------------- */
+
+/**
+ * Backend Calendar
+ *
+ * This module implements the table calendar view of backend.
+ *
+ * @module BackendCalendarTableView
+ */
+window.BackendCalendarTableView = window.BackendCalendarTableView || {};
+
+(function(exports) {
+    'use strict';
+
+    function _bindEventHandlers() {
+
+    }
+
+    exports.initialize = function() {
+
+    };
+
+})(window.BackendCalendarTableView);
