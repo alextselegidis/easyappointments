557c28243305d8f822c7ae04842d7a375c7e4ae6
alext <alextselegidis@gmail.com>
2016-11-13 12:25:02 +0100

    PHP sessions will be stored in the "storage" directory (fixes #241)

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -292,9 +292,9 @@ $config['encryption_key'] = Config::BASE_URL;
 |
 */
 $config['sess_driver']             = 'files';
-$config['sess_cookie_name']		   = 'ci_session';
+$config['sess_cookie_name']		   = 'ea_session';
 $config['sess_expiration']		   = 7200;
-$config['sess_save_path']          = NULL;
+$config['sess_save_path']          = __DIR__ . '/../../storage/sessions';
 $config['sess_match_ip']		   = FALSE;
 $config['sess_time_to_update']	   = 300;
 $config['sess_regenerate_destroy'] = FALSE;
