b22240e38e6ef9748e9518e8af2516cdd0b4171a
alext <alextselegidis@gmail.com>
2017-06-11 21:46:30 +0200

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var gulp = require('gulp'),
-    exec = require('child_process').execSync,
-    del = require('del'),
-    fs = require('fs-extra'),
-    path = require('path'),
-    zip = require('zip-dir');
+const gulp = require('gulp');
+const exec = require('child_process').execSync;
+const del = require('del');
+const fs = require('fs-extra');
+const path = require('path');
+const zip = require('zip-dir');
 
 /**
  * Install and copy the required files from the "composer_modules" directory.
@@ -13,32 +13,26 @@ var gulp = require('gulp'),
  */
 gulp.task('composer', function() {
     del.sync([
-        './composer_modules',
         './src/vendor/**/*',
         '!./src/vendor/index.html'
     ]);
 
-    exec('composer update && composer install --prefer-dist', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-    });
-
     return gulp.src([
-        'composer_modules/**/*',
-        '!composer_modules/**/demo{,/**}',
-        '!composer_modules/**/{demo,docs,examples,test,extras,language}{,/**}',
-        '!composer_modules/**/{composer.json,composer.lock,.gitignore}',
-        '!composer_modules/**/{*.yml,*.md}',
-        '!composer_modules/bin{,/**}',
-        '!composer_modules/codeigniter{,/**}',
-        '!composer_modules/doctrine{,/**}',
-        '!composer_modules/myclabs{,/**}',
-        '!composer_modules/phpdocumentor{,/**}',
-        '!composer_modules/phpspec{,/**}',
-        '!composer_modules/phpunit{,/**}',
-        '!composer_modules/sebastian{,/**}',
-        '!composer_modules/symfony{,/**}',
-        '!composer_modules/webmozart{,/**}'
+        'vendor/**/*',
+        '!vendor/**/demo{,/**}',
+        '!vendor/**/{demo,docs,examples,test,tests,extras,language}{,/**}',
+        '!vendor/**/{composer.json,composer.lock,.gitignore}',
+        '!vendor/**/{*.yml,*.md}',
+        '!vendor/bin{,/**}',
+        '!vendor/codeigniter{,/**}',
+        '!vendor/doctrine{,/**}',
+        '!vendor/myclabs{,/**}',
+        '!vendor/phpdocumentor{,/**}',
+        '!vendor/phpspec{,/**}',
+        '!vendor/phpunit{,/**}',
+        '!vendor/sebastian{,/**}',
+        '!vendor/symfony{,/**}',
+        '!vendor/webmozart{,/**}'
     ])
         .pipe(gulp.dest('./src/vendor/'));
 });
@@ -100,7 +94,7 @@ gulp.task('doc', function(done) {
     fs.removeSync('doc/plato');
     fs.mkdirSync('doc/plato');
 
-    var commands = [
+    const commands = [
         'php rsc/apigen.phar generate ' +
             '-s "src/application/controllers,src/application/models,src/application/libraries" ' +
             '-d "doc/apigen" --exclude "*external*" --tree --todo --template-theme "bootstrap"',
