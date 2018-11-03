333838b6352b09ce8a184f440d373feb55a08ee0
alext <alextselegidis@gmail.com>
2018-11-03 16:15:52 +0100

    Integrated gulp tasks in for script and style minification.

--- gulpfile.js
+++ gulpfile.js
@@ -1,114 +1,35 @@
-const gulp = require('gulp');
-const exec = require('child_process').execSync;
-const del = require('del');
-const fs = require('fs-extra');
-const path = require('path');
-const zip = require('zip-dir');
-
-/**
- * Install and copy the required files from the "composer_modules" directory.
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
  *
- * Composer needs to be installed and configured in order for this command to
- * work properly.
- */
-gulp.task('composer', function() {
-    del.sync([
-        './src/vendor/**/*',
-        '!./src/vendor/index.html'
-    ]);
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2018, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.4.0
+ * ---------------------------------------------------------------------------- */
 
-    return gulp.src([
-        'vendor/**/*',
-        '!vendor/**/demo{,/**}',
-        '!vendor/**/{demo,docs,examples,test,tests,extras,language}{,/**}',
-        '!vendor/**/{composer.json,composer.lock,.gitignore}',
-        '!vendor/**/{*.yml,*.md,*phpunit*,*.mdown}',
-        '!vendor/bin{,/**}',
-        '!vendor/codeigniter{,/**}',
-        '!vendor/doctrine{,/**}',
-        '!vendor/myclabs{,/**}',
-        '!vendor/phpdocumentor{,/**}',
-        '!vendor/phpspec{,/**}',
-        '!vendor/phpunit{,/**}',
-        '!vendor/sebastian{,/**}',
-        '!vendor/symfony{,/**}',
-        '!vendor/webmozart{,/**}'
-    ])
-        .pipe(gulp.dest('./src/vendor/'));
-});
+// Gulp instance and plugins.
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
 
-/**
- * Build the project and create an easyappointments.zip file ready for distribution.
- */
-gulp.task('build', function(done) {
-    del.sync([
-        'build',
-        'easyappointments-0.0.0.zip'
-    ]);
+// Gulp error handling.
+const source = gulp.src;
+gulp.src = function() {
+    return source.apply(gulp, arguments)
+        .pipe(plugins.plumber({
+            errorHandler: plugins.notify.onError('Error: <%= error.message %>')
+        }));
+};
 
-    fs.copySync('src', 'build');
-    fs.removeSync('build/config.php');
-    fs.copySync('CHANGELOG.md', 'build/CHANGELOG.md');
-    fs.copySync('README.md', 'build/README.md');
-    fs.copySync('LICENSE', 'build/LICENSE');
-
-    del.sync([
-        'build/storage/uploads/*',
-        '!build/storage/uploads/index.html'
-    ]);
-
-    del.sync([
-        'build/storage/logs/*',
-        '!build/storage/logs/index.html'
-    ]);
-
-    del.sync([
-        'build/storage/sessions/*',
-        '!build/storage/sessions/.htaccess',
-        '!build/storage/sessions/index.html'
-    ]);
-
-    del.sync([
-        'build/storage/cache/*',
-        '!build/storage/cache/.htaccess',
-        '!build/storage/cache/index.html'
-    ]);
-
-    zip('build', { saveTo: 'easyappointments-0.0.0.zip' }, function (err, buffer) {
-        if (err)
-            console.log('Zip Error', err);
-
-        done();
-    });
-});
-
-/**
- * Generate code documentation.
- */
-gulp.task('doc', function(done) {
-    fs.removeSync('docs/apigen/html');
-    fs.mkdirSync('docs/apigen/html');
-    fs.removeSync('docs/jsdoc/html');
-    fs.mkdirSync('docs/jsdoc/html');
-    fs.removeSync('docs/plato/html');
-    fs.mkdirSync('docs/plato/html');
-
-    const commands = [
-        'php docs/apigen/apigen.phar generate ' +
-            '-s "src/application/controllers,src/application/models,src/application/libraries" ' +
-            '-d "docs/apigen/html" --exclude "*external*" --tree --todo --template-theme "bootstrap"',
-
-        path.join('.', 'node_modules', '.bin', 'jsdoc') + ' "src/assets/js" -d "doc/jsdoc/html"',
-
-        path.join('.', 'node_modules', '.bin', 'plato') + ' -r -d "docs/plato/html" "src/assets/js"'
-    ];
-
-    commands.forEach(function(command) {
-        exec(command, function (err, stdout, stderr) {
-            console.log(stdout);
-            console.log(stderr);
-        });
-    });
-
-    done();
-});
+// Define gulp tasks.
+gulp.task('build', require('./tools/gulp/build')(gulp, plugins));
+gulp.task('clean', require('./tools/gulp/clean')(gulp, plugins));
+gulp.task('composer', require('./tools/gulp/composer')(gulp, plugins));
+gulp.task('dev', require('./tools/gulp/dev')(gulp, plugins));
+gulp.task('docs', require('./tools/gulp/docs')(gulp, plugins));
+gulp.task('scripts', require('./tools/gulp/scripts')(gulp, plugins));
+gulp.task('styles', require('./tools/gulp/styles')(gulp, plugins));
+gulp.task('tests', require('./tools/gulp/tests')(gulp, plugins));
+gulp.task('watch', require('./tools/gulp/watch')(gulp, plugins));
+gulp.task('default', ['dev']);
---
+++ tools/gulp/composer.js
@@ -0,0 +1,46 @@
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2018, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.4.0
+ * ---------------------------------------------------------------------------- */
+
+const del = require('del');
+
+/**
+ * Install and copy the required files from the "composer_modules" directory.
+ *
+ * Composer needs to be installed and configured in order for this command to
+ * work properly.
+ */
+module.exports = (gulp, plugins) => {
+    return () => {
+        del.sync([
+            './src/vendor/**/*',
+            '!./src/vendor/index.html'
+        ]);
+
+        return gulp.src([
+            'vendor/**/*',
+            '!vendor/**/demo{,/**}',
+            '!vendor/**/{demo,docs,examples,test,tests,extras,language}{,/**}',
+            '!vendor/**/{composer.json,composer.lock,.gitignore}',
+            '!vendor/**/{*.yml,*.md,*phpunit*,*.mdown}',
+            '!vendor/bin{,/**}',
+            '!vendor/codeigniter{,/**}',
+            '!vendor/doctrine{,/**}',
+            '!vendor/myclabs{,/**}',
+            '!vendor/phpdocumentor{,/**}',
+            '!vendor/phpspec{,/**}',
+            '!vendor/phpunit{,/**}',
+            '!vendor/sebastian{,/**}',
+            '!vendor/symfony{,/**}',
+            '!vendor/webmozart{,/**}'
+        ])
+            .pipe(gulp.dest('./src/vendor/'));
+    };
+};
