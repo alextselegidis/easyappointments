9996b38e1e05610b9b7745571dd63210385d4de0
Johan Idstam <johan@idstam.com>
2020-05-18 20:10:40 +0200

    Swedish for migration_lang.php

---
+++ application/language/swedish/migration_lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014-2018, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['migration_none_found'] = 'Hittade inga migrationer.';
+$lang['migration_not_found'] = 'Hittade inga migrationer med versionsnummer: %s.';
+$lang['migration_sequence_gap'] = 'Det är glapp i migrationssekvensen i version nummer: %s.';
+$lang['migration_multiple_version'] = 'Det finn flera migrationer med samma versionsnummer: %s.';
+$lang['migration_class_doesnt_exist'] = 'Migrationsklassen "%s" hittades inte.';
+$lang['migration_missing_up_method'] = 'Migrationsklassen "%s" saknar "up"-metoden.';
+$lang['migration_missing_down_method'] = 'Migrationsklassen "%s" saknar "down"-metoden.';
+$lang['migration_invalid_filename'] = 'Migrationen "%s" har ogiltigt filnamn.';
