92680cc60ca83bb2476890694a3a54f635e5fa61
Vitor Mattos <vitor@php.rio>
2021-04-10 00:06:52 -0300

    Implement integration tests

---
+++ tests/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+class TestCase extends CIPHPUnitTestCase
+{
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->resetInstance();
+        $this->CI->load->dbutil();
+        if ($this->CI->dbutil->database_exists($this->CI->db->database . '_test')) {
+            return;
+        }
+        $this->CI->db->simple_query('CREATE DATABASE ' . $this->CI->db->database . '_test');
+        $this->CI->db->simple_query('USE ' . $this->CI->db->database . '_test');
+        $this->request(null, 'console/install');
+    }
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        $CI =& get_instance();
+
+        if (!$CI->db->trans_status()) {
+            $CI->db->trans_begin();
+        }
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->CI->db->trans_rollback();
+        $this->CI->db->simple_query('DROP DATABASE ' . $this->CI->db->database . '_test');
+    }
+}
---
+++ tests/controllers/api/v1/ServicesTest.php
@@ -0,0 +1,58 @@
+<?php
+
+class ServicesTest extends TestCase
+{
+	public function testGetWithoutId()
+	{
+		$output = $this->request('GET', 'api/v1/services/get');
+		$expected = <<<INPUT
+		[
+		    {
+		        "id": 1,
+		        "name": "Service",
+		        "duration": 30,
+		        "price": 0,
+		        "currency": "",
+		        "description": null,
+		        "location": null,
+		        "availabilitiesType": "flexible",
+		        "attendantsNumber": 1,
+		        "categoryId": null
+		    }
+		]
+		INPUT;
+		$this->assertEquals($expected, $output);
+	}
+
+	public function testGetWithId()
+	{
+		$output = $this->request('GET', 'api/v1/services/get/1');
+		$expected = <<<INPUT
+		{
+		    "id": 1,
+		    "name": "Service",
+		    "duration": 30,
+		    "price": 0,
+		    "currency": "",
+		    "description": null,
+		    "location": null,
+		    "availabilitiesType": "flexible",
+		    "attendantsNumber": 1,
+		    "categoryId": null
+		}
+		INPUT;
+		$this->assertEquals($expected, $output);
+	}
+
+	public function testGetWithInvalidId()
+	{
+		$output = $this->request('GET', 'api/v1/services/get/invalid');
+		$expected = <<<INPUT
+		{
+		    "code": 404,
+		    "message": "The requested record was not found!"
+		}
+		INPUT;
+		$this->assertEquals($expected, $output);
+	}
+}
