81ebc63a18dccf1184a4375b877e690bb3e8ef85
Vitor Mattos <vitor@php.rio>
2021-04-10 11:56:21 -0300

    Use dataProvider

--- tests/controllers/api/v1/ServicesTest.php
+++ tests/controllers/api/v1/ServicesTest.php
@@ -2,90 +2,100 @@
 
 class ServicesTest extends TestCase
 {
-	public function testGetWithoutId()
+	/**
+	 * @dataProvider providerRequest
+	 */
+	public function testRequest($method, $route, $requestBody, $expected)
 	{
-		$output = $this->request('GET', 'api/v1/services/get');
-		$expected = <<<INPUT
-		[
-		    {
-		        "id": 1,
-		        "name": "Service",
-		        "duration": 30,
-		        "price": 0,
-		        "currency": "",
-		        "description": null,
-		        "location": null,
-		        "availabilitiesType": "flexible",
-		        "attendantsNumber": 1,
-		        "categoryId": null
-		    }
-		]
-		INPUT;
+		$output = $this->request($method, $route, $requestBody);
 		$this->assertEquals($expected, $output);
 	}
-
-	public function testGetWithId()
+	
+	public function providerRequest()
 	{
-		$output = $this->request('GET', 'api/v1/services/get/1');
-		$expected = <<<INPUT
-		{
-		    "id": 1,
-		    "name": "Service",
-		    "duration": 30,
-		    "price": 0,
-		    "currency": "",
-		    "description": null,
-		    "location": null,
-		    "availabilitiesType": "flexible",
-		    "attendantsNumber": 1,
-		    "categoryId": null
-		}
-		INPUT;
-		$this->assertEquals($expected, $output);
-	}
-
-	public function testGetWithInvalidId()
-	{
-		$output = $this->request('GET', 'api/v1/services/get/invalid');
-		$expected = <<<INPUT
-		{
-		    "code": 404,
-		    "message": "The requested record was not found!"
-		}
-		INPUT;
-		$this->assertEquals($expected, $output);
-	}
-
-	public function testPostSuccess()
-	{
-		$requestBody = <<<INPUT
-		{
-		    "name": "Service 02",
-		    "duration": 30,
-		    "price": 0,
-		    "currency": "",
-		    "description": null,
-		    "location": null,
-		    "availabilitiesType": "flexible",
-		    "attendantsNumber": 1,
-		    "categoryId": null
-		}
-		INPUT;
-		$output = $this->request('POST', 'api/v1/services/post', $requestBody);
-		$expected = <<<INPUT
-		{
-		    "id": 2,
-		    "name": "Service 02",
-		    "duration": 30,
-		    "price": 0,
-		    "currency": "",
-		    "description": null,
-		    "location": null,
-		    "availabilitiesType": "flexible",
-		    "attendantsNumber": 1,
-		    "categoryId": null
-		}
-		INPUT;
-		$this->assertEquals($expected, $output);
+		return [
+			[
+				'GET',
+				'api/v1/services/get',
+				null,
+				<<<EXPECTED
+				[
+				    {
+				        "id": 1,
+				        "name": "Service",
+				        "duration": 30,
+				        "price": 0,
+				        "currency": "",
+				        "description": null,
+				        "location": null,
+				        "availabilitiesType": "flexible",
+				        "attendantsNumber": 1,
+				        "categoryId": null
+				    }
+				]
+				EXPECTED
+			],
+			[
+				'GET',
+				'api/v1/services/get/1',
+				null,
+				<<<EXPECTED
+				{
+				    "id": 1,
+				    "name": "Service",
+				    "duration": 30,
+				    "price": 0,
+				    "currency": "",
+				    "description": null,
+				    "location": null,
+				    "availabilitiesType": "flexible",
+				    "attendantsNumber": 1,
+				    "categoryId": null
+				}
+				EXPECTED
+			],
+			[
+				'GET',
+				'api/v1/services/get/invalid',
+				null,
+				<<<EXPECTED
+				{
+				    "code": 404,
+				    "message": "The requested record was not found!"
+				}
+				EXPECTED
+			],
+			[
+				'POST',
+				'api/v1/services/post',
+				<<<REQUEST_BODY
+				{
+				    "name": "Service 02",
+				    "duration": 30,
+				    "price": 0,
+				    "currency": "",
+				    "description": null,
+				    "location": null,
+				    "availabilitiesType": "flexible",
+				    "attendantsNumber": 1,
+				    "categoryId": null
+				}
+				REQUEST_BODY,
+				<<<EXPECTED
+				{
+				    "id": 2,
+				    "name": "Service 02",
+				    "duration": 30,
+				    "price": 0,
+				    "currency": "",
+				    "description": null,
+				    "location": null,
+				    "availabilitiesType": "flexible",
+				    "attendantsNumber": 1,
+				    "categoryId": null
+				}
+				EXPECTED
+			]
+		];
 	}
 }
