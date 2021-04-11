84aed85b8ee9eefa802204d6887297e35f7b35f0
Vitor Mattos <vitor@php.rio>
2021-04-11 17:22:44 -0300

    Add customer model test

---
+++ tests/model/Customers_modelTest.php
@@ -0,0 +1,66 @@
+<?php
+
+class Customers_modelTest extends TestCase
+{
+    /** @var Services_model */
+    private $model;
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->resetInstance();
+        $this->CI->load->model('Customers_model');
+        $this->model = $this->CI->Customers_model;
+    }
+
+    /**
+     * @dataProvider providerModel
+     */
+    public function testModel($method, $arguments, $expected)
+    {
+        $output = call_user_func([$this->model, $method], ...$arguments);
+        $this->assertEquals($expected, $output);
+    }
+
+    public function providerModel()
+    {
+        return [
+            [
+                'add',
+                [
+                    [
+                        'first_name' => 'John',
+                        'last_name' => 'Doe',
+                        'email' => 'john@example.org',
+                        'phone_number' => '123456789',
+                    ]
+                ],
+                5
+            ],
+            [
+                'add',
+                [
+                    [
+                        'id' => 5,
+                        'first_name' => 'Jane',
+                        'last_name' => 'Doe',
+                        'email' => 'Jane@example.org',
+                        'phone_number' => '987654321',
+                    ]
+                ],
+                5
+            ],
+            [
+                'exists',
+                [
+                    [
+                        'first_name' => 'Jane',
+                        'last_name' => 'Doe',
+                        'email' => 'Jane@example.org',
+                        'phone_number' => '987654321'
+                    ]
+                ],
+                true
+            ],
+        ];
+    }
+}
