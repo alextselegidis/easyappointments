41c92a2a4fb59d7310aaa74a20657cf902e23d68
Vitor Mattos <vitor@php.rio>
2021-04-12 21:03:10 -0300

    Tunning and improvements in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,36 +2,25 @@
 
 class TestCase extends CIPHPUnitTestCase
 {
-    public function resetInstance($use_my_controller = false)
-    {
-        parent::resetInstance($use_my_controller);
-        $this->CI->load->dbutil();
-        if ($this->CI->dbutil->database_exists($_ENV['DB_NAME'])) {
-            $this->switchDatabase();
-            return;
-        }
-        $this->CI->db->simple_query('CREATE DATABASE ' . $_ENV['DB_NAME']);
-        $this->switchDatabase();
-        $this->request(null, 'console/install');
-    }
-
-    private function switchDatabase()
-    {
-        require APPPATH.'config/database.php';
-        $db['default']['database'] = $_ENV['DB_NAME'];
-        $this->CI->load->database($db['default'], TRUE);
-    }
-
     public function setUp(): void
     {
         parent::setUp();
-        $this->resetInstance();
+        parent::resetInstance();
+        if ($this->CI->db->table_exists($this->CI->db->dbprefix('settings'))) {
+            return;
+        }
+        $this->request(null, 'console/install');
+    }
+
+    public static function setUpBeforeClass(): void
+    {
+        $CI =& get_instance();
+        $CI->db->trans_start();
     }
 
     public static function tearDownAfterClass(): void
     {
-        parent::tearDownAfterClass();
         $CI =& get_instance();
-        $CI->db->simple_query('DROP DATABASE ' . $_ENV['DB_NAME']);
+        $CI->db->trans_rollback();
     }
 }
