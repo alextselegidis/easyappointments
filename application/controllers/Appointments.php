7740a8f97c0f0783070dc47e2f96a75394050888
Vitor Mattos <vitor@php.rio>
2021-04-13 23:37:57 -0300

    Fix condition to enable tabs

--- application/controllers/Appointments.php
+++ application/controllers/Appointments.php
@@ -169,9 +169,9 @@ class Appointments extends EA_Controller {
             $variables = [
                 'active_step' => $active_step,
                 'show_step' => [
-                    1 => !$service && !$provider,
+                    1 => !$service || !$provider,
                     2 => true,
-                    3 => !$service && !$provider && !$user,
+                    3 => !$user,
                     4 => true
                 ],
                 'available_services' => $available_services,
