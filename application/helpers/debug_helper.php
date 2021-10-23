c117eda10100671bc3d5cd637058cc9b95bb165e
Alex Tselegidis <alextselegidis@gmail.com>
2021-10-23 13:29:31 +0200

    Set the right @since value for the new helper files

--- application/helpers/debug_helper.php
+++ application/helpers/debug_helper.php
@@ -8,7 +8,7 @@
  * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
  * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
  * @link        http://easyappointments.org
- * @since       v1.1.0
+ * @since       v1.5.0
  * ---------------------------------------------------------------------------- */
 
 if ( ! function_exists('dd'))
