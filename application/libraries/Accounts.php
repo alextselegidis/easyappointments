dc50f8dee1c063aad9ca62dc7eaf4d4ef5c84ca3
Alex Tselegidis <alextselegidis@gmail.com>
2021-11-02 09:24:28 +0100

    Updated @since tag

--- application/libraries/Accounts.php
+++ application/libraries/Accounts.php
@@ -8,7 +8,7 @@
  * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
  * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
  * @link        http://easyappointments.org
- * @since       v1.4.0
+ * @since       v1.5.0
  * ---------------------------------------------------------------------------- */
 
 
