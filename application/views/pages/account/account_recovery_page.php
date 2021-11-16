4a49800674610fd1991ac63fa77cb622790dd57c
alextselegidis <alextselegidis@gmail.com>
2021-11-16 10:22:24 +0100

    Minor formatting

--- application/views/pages/account/account_recovery_page.php
+++ application/views/pages/account/account_recovery_page.php
@@ -3,9 +3,13 @@
 <?php section('content') ?>
 
 <h2><?= lang('forgot_your_password') ?></h2>
+
 <p><?= lang('type_username_and_email_for_new_password') ?></p>
+
 <hr>
+
 <div class="alert d-none"></div>
+
 <form>
     <div class="form-group">
         <label for="username"><?= lang('username') ?></label>
@@ -24,7 +28,8 @@
     </button>
 
     <a href="<?= site_url('user/login') ?>" class="user-login">
-        <?= lang('go_to_login') ?></a>
+        <?= lang('go_to_login') ?>
+    </a>
 </form>
 
 <?php section('content') ?>
