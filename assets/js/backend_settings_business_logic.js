2d1fb357c82ae97589123d6f1edaf32534e93c0a
alextselegidis <alextselegidis@gmail.com>
2021-11-18 08:49:23 +0100

    Ported the business logic settings functionality to a new standalone page

---
+++ assets/js/backend_settings_business_logic.js
@@ -0,0 +1,137 @@
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+window.BackendSettingsBusinessLogic = window.BackendSettingsBusinessLogic || {};
+
+/**
+ * Backend Settings
+ *
+ * Contains the functionality of the backend settings page. Can either work for system or user settings,
+ * but the actions allowed to the user are restricted to his role (only admin has full privileges).
+ *
+ * @module BackendSettingsBusinessLogic
+ */
+(function (exports) {
+    'use strict';
+
+    // Constants
+    exports.SETTINGS_SYSTEM = 'SETTINGS_SYSTEM';
+
+    /**
+     * Use this WorkingPlan class instance to perform actions on the page's working plan tables.
+     *
+     * @type {WorkingPlan}
+     */
+    exports.wp = {};
+
+    /**
+     * Tab settings object.
+     *
+     * @type {Object}
+     */
+    var settings = {};
+
+    /**
+     * Initialize Page
+     *
+     * @param {bool} defaultEventHandlers Optional (true), determines whether to bind the default event handlers.
+     */
+    exports.initialize = function (defaultEventHandlers) {
+        defaultEventHandlers = defaultEventHandlers || true;
+
+        // Apply setting values from database.
+        var workingPlan = {};
+
+        GlobalVariables.settings.system.forEach(function (setting) {
+            $('input[data-field="' + setting.name + '"]').val(setting.value);
+            $('select[data-field="' + setting.name + '"]').val(setting.value);
+
+            if (setting.name === 'company_working_plan') {
+                workingPlan = $.parseJSON(setting.value);
+            }
+        });
+
+        exports.wp = new WorkingPlan();
+        exports.wp.setup(workingPlan);
+        exports.wp.timepickers(false);
+
+        // Set default settings helper.
+        settings = new SystemSettingsBusinessLogicHelper();
+
+        if (defaultEventHandlers) {
+            bindEventHandlers();
+            var $link = $('#settings-page .nav li').not('.d-none').first().find('a');
+            $link.tab('show');
+        }
+
+        // Apply Privileges
+        if (GlobalVariables.user.privileges.system_settings.edit === false) {
+            $('#business-logic').find('select, input, textarea').prop('readonly', true);
+            $('#business-logic').find('button').prop('disabled', true);
+        }
+
+        Backend.placeFooterToBottom();
+    };
+
+    /**
+     * Bind the backend/settings default event handlers.
+     *
+     * This method depends on the backend/settings html, so do not use this method on a different page.
+     */
+    function bindEventHandlers() {
+        exports.wp.bindEventHandlers();
+
+        /**
+         * Event: Save Settings Button "Click"
+         *
+         * Store the setting changes into the database.
+         */
+        $('.save-settings').on('click', function () {
+            var data = settings.get();
+            settings.save(data);
+        });
+
+        /**
+         * Event: Apply Global Working Plan
+         */
+        $('#apply-global-working-plan').on('click', function () {
+            var buttons = [
+                {
+                    text: EALang.cancel,
+                    click: function () {
+                        $('#message-box').dialog('close');
+                    }
+                },
+                {
+                    text: 'OK',
+                    click: function () {
+                        var url = GlobalVariables.baseUrl + '/index.php/backend_api/ajax_apply_global_working_plan';
+
+                        var data = {
+                            csrfToken: GlobalVariables.csrfToken,
+                            working_plan: JSON.stringify(exports.wp.get())
+                        };
+
+                        $.post(url, data)
+                            .done(function () {
+                                Backend.displayNotification(EALang.working_plans_got_updated);
+                            })
+                            .always(function () {
+                                $('#message-box').dialog('close');
+                            });
+                    }
+                }
+            ];
+
+            GeneralFunctions.displayMessageBox(EALang.working_plan, EALang.overwrite_existing_working_plans, buttons);
+        });
+    }
+})(window.BackendSettingsBusinessLogic);
---
+++ assets/js/backend_settings_business_logic_helper.js
@@ -0,0 +1,123 @@
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+(function () {
+    'use strict';
+
+    /**
+     * "System Settings" Tab Helper Class
+     *
+     * @class SystemSettingsBusinessLogicHelper
+     */
+    var SystemSettingsBusinessLogicHelper = function () {};
+
+    /**
+     * Save the system settings.
+     *
+     * This method is run after changes are detected on the tab input fields.
+     *
+     * @param {Array} settings Contains the system settings data.
+     */
+    SystemSettingsBusinessLogicHelper.prototype.save = function (settings) {
+        if (!this.validate()) {
+            return; // Validation failed, do not proceed.
+        }
+
+        var url = GlobalVariables.baseUrl + '/index.php/backend_api/ajax_save_settings';
+
+        var data = {
+            csrfToken: GlobalVariables.csrfToken,
+            settings: JSON.stringify(settings),
+            type: BackendSettingsBusinessLogic.SETTINGS_SYSTEM
+        };
+
+        $.post(url, data).done(function () {
+            Backend.displayNotification(EALang.settings_saved);
+
+            // Update the logo title on the header.
+            $('#header-logo span').text($('#company-name').val());
+
+            // Update book_advance_timeout preview
+            var totalMinutes = $('#book-advance-timeout').val();
+            var hours = Math.floor(totalMinutes / 60);
+            var minutes = totalMinutes % 60;
+            $('#book-advance-timeout-helper').text(
+                EALang.book_advance_timeout_hint.replace(
+                    '{$limit}',
+                    ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2)
+                )
+            );
+
+            // We need to refresh the working plan.
+            var workingPlan = BackendSettingsBusinessLogic.wp.get();
+            BackendSettingsBusinessLogic.wp.setup(workingPlan);
+            BackendSettingsBusinessLogic.wp.timepickers(false);
+        });
+    };
+
+    /**
+     * Prepare the system settings array.
+     *
+     * This method uses the DOM elements of the backend/settings page, so it can't be used in another page.
+     *
+     * @return {Array} Returns the system settings array.
+     */
+    SystemSettingsBusinessLogicHelper.prototype.get = function () {
+        var settings = [];
+
+        // Business Logic Tab
+
+        settings.push({
+            name: 'company_working_plan',
+            value: JSON.stringify(BackendSettingsBusinessLogic.wp.get())
+        });
+
+        settings.push({
+            name: 'book_advance_timeout',
+            value: $('#book-advance-timeout').val()
+        });
+
+        return settings;
+    };
+
+    /**
+     * Validate the settings data.
+     *
+     * If the validation fails then display a message to the user.
+     *
+     * @return {Boolean} Returns the validation result.
+     */
+    SystemSettingsBusinessLogicHelper.prototype.validate = function () {
+        $('#business-logic .has-error').removeClass('has-error');
+
+        try {
+            // Validate required fields.
+            var missingRequired = false;
+            $('#business-logic .required').each(function (index, requiredField) {
+                if (!$(requiredField).val()) {
+                    $(requiredField).closest('.form-group').addClass('has-error');
+                    missingRequired = true;
+                }
+            });
+
+            if (missingRequired) {
+                throw new Error(EALang.fields_are_required);
+            }
+
+            return true;
+        } catch (error) {
+            Backend.displayNotification(error.message);
+            return false;
+        }
+    };
+
+    window.SystemSettingsBusinessLogicHelper = SystemSettingsBusinessLogicHelper;
+})();
