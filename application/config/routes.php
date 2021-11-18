81044eb9c6cfdfc2ab07c0610682016ae076893b
Alex Tselegidis <alextselegidis@gmail.com>
2021-11-18 17:17:08 +0100

    Corrected issue with routing and CORS while running from the terminal

--- application/config/routes.php
+++ application/config/routes.php
@@ -66,7 +66,7 @@ $route['translate_uri_dashes'] = FALSE;
 |
 */
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS')
+if (isset($_SERVER) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS')
 {
     header('Access-Control-Allow-Origin: *');
 
