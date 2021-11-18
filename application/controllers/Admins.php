d6b294a12bc3ad360ef98783551152cb800e9beb
alextselegidis <alextselegidis@gmail.com>
2021-11-18 06:11:18 +0100

    Created a new admins resource controller

---
+++ application/controllers/Admins.php
@@ -0,0 +1,173 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
+ * @license     https://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        https://easyappointments.org
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+/**
+ * Admins controller.
+ *
+ * Handles the admins related operations.
+ *
+ * @package Controllers
+ */
+class Admins extends EA_Controller {
+    /**
+     * Admins constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('admins_model');
+        $this->load->model('roles_model');
+
+        $this->load->library('accounts');
+        $this->load->library('timezones');
+    }
+
+    /**
+     * Render the backend admins page.
+     *
+     * On this page admin users will be able to manage admins, which are eventually selected by customers during the 
+     * booking process.
+     */
+    public function index()
+    {
+        session(['dest_url' => site_url('admins')]);
+
+        if (cannot('view', 'users'))
+        {
+            show_error('Forbidden', 403);
+        }
+
+        $user_id = session('user_id');
+
+        $role_slug = session('role_slug');
+
+        $this->load->view('pages/admins/admins_page', [
+            'page_title' => lang('admins'),
+            'active_menu' => PRIV_USERS,
+            'user_display_name' => $this->accounts->get_user_display_name($user_id),
+            'timezones' => $this->timezones->to_array(),
+            'privileges' => $this->roles_model->get_permissions_by_slug($role_slug),
+        ]);
+    }
+
+    /**
+     * Filter admins by the provided keyword.
+     */
+    public function search()
+    {
+        try
+        {
+            if (cannot('view', 'users'))
+            {
+                show_error('Forbidden', 403);
+            }
+
+            $keyword = request('keyword', '');
+
+            $order_by = 'first_name ASC, last_name ASC, email ASC';
+
+            $limit = request('limit', 1000);
+            
+            $offset = 0;
+
+            $admins = $this->admins_model->search($keyword, $limit, $offset, $order_by);
+
+            json_response($admins);
+        }
+        catch (Throwable $e)
+        {
+            json_exception($e);
+        }
+    }
+
+    /**
+     * Create a admin.
+     */
+    public function create()
+    {
+        try
+        {
+            $admin = json_decode(request('admin'), TRUE);
+
+            if (cannot('add', 'users'))
+            {
+                show_error('Forbidden', 403);
+            }
+
+            $admin_id = $this->admins_model->save($admin);
+
+            json_response([
+                'success' => TRUE,
+                'id' => $admin_id
+            ]);
+        }
+        catch (Throwable $e)
+        {
+            json_exception($e);
+        }
+    }
+
+    /**
+     * Update a admin.
+     */
+    public function update()
+    {
+        try
+        {
+            $admin = json_decode(request('admin'), TRUE);
+
+            if (cannot('edit', 'users'))
+            {
+                show_error('Forbidden', 403);
+            }
+
+            $admin_id = $this->admins_model->save($admin);
+
+            json_response([
+                'success' => TRUE,
+                'id' => $admin_id
+            ]);
+        }
+        catch (Throwable $e)
+        {
+            json_exception($e);
+        }
+    }
+
+    /**
+     * Remove a admin.
+     */
+    public function destroy()
+    {
+        try
+        {
+            if (cannot('delete', 'users'))
+            {
+                show_error('Forbidden', 403);
+            }
+
+            $admin_id = request('admin_id');
+
+            $this->admins_model->delete($admin_id);
+
+            json_response([
+                'success' => TRUE,
+            ]);
+        }
+        catch (Throwable $e)
+        {
+            json_exception($e);
+        }
+    }
+}
---
+++ assets/js/backend_admins.js
@@ -0,0 +1,107 @@
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+window.BackendAdmins = window.BackendAdmins || {};
+
+/**
+ * Backend Admins
+ *
+ * This module handles the js functionality of the admins backend page. It uses three other
+ * classes (defined below) in order to handle the admin, admin and secretary record types.
+ *
+ * @module BackendAdmins
+ */
+(function (exports) {
+    'use strict';
+
+    /**
+     * Minimum Password Length
+     *
+     * @type {Number}
+     */
+    exports.MIN_PASSWORD_LENGTH = 7;
+
+    /**
+     * Contains the current tab record methods for the page.
+     *
+     * @type {AdminsHelper}
+     */
+    var helper = {};
+
+    /**
+     * Initialize the backend admins page.
+     *
+     * @param {Boolean} defaultEventHandlers (OPTIONAL) Whether to bind the default event handlers.
+     */
+    exports.initialize = function (defaultEventHandlers) {
+        defaultEventHandlers = defaultEventHandlers || true;
+
+        // Instantiate default helper object (admin).
+        helper = new AdminsHelper();
+        helper.resetForm();
+        helper.filter('');
+        helper.bindEventHandlers();
+
+        // Bind event handlers.
+        if (defaultEventHandlers) {
+            bindEventHandlers();
+        }
+    };
+
+    /**
+     * Binds the default backend admins event handlers. Do not use this method on a different
+     * page because it needs the backend admins page DOM.
+     */
+    function bindEventHandlers() {
+        /**
+         * Event: Admin Username "Blur"
+         *
+         * When the admin leaves the username input field we will need to check if the username
+         * is not taken by another record in the system.
+         */
+        $('#admin-username').focusout(function () {
+            var $input = $(this);
+
+            if ($input.prop('readonly') === true || $input.val() === '') {
+                return;
+            }
+
+            var adminId = $input.parents().eq(2).find('.record-id').val();
+
+            if (!adminId) {
+                return;
+            }
+
+            var url = GlobalVariables.baseUrl + '/index.php/backend_api/ajax_validate_username';
+
+            var data = {
+                csrfToken: GlobalVariables.csrfToken,
+                username: $input.val(),
+                user_id: adminId
+            };
+
+            $.post(url, data).done(function (response) {
+                if (response.is_valid === 'false') {
+                    $input.closest('.form-group').addClass('has-error');
+                    $input.attr('already-exists', 'true');
+                    $input.parents().eq(3).find('.form-message').text(EALang.username_already_exists);
+                    $input.parents().eq(3).find('.form-message').show();
+                } else {
+                    $input.closest('.form-group').removeClass('has-error');
+                    $input.attr('already-exists', 'false');
+                    if ($input.parents().eq(3).find('.form-message').text() === EALang.username_already_exists) {
+                        $input.parents().eq(3).find('.form-message').hide();
+                    }
+                }
+            });
+        });
+    }
+})(window.BackendAdmins);
