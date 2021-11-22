a622f2f295ee8fe66a95170d27ce28d77d9d53c1
alextselegidis <alextselegidis@gmail.com>
2021-11-22 07:36:05 +0100

    Ported the current user settings functionality to a new standalone page

---
+++ application/controllers/settings/Current_user.php
@@ -0,0 +1,110 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
+ * @license     https://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        https://easyappointments.org
+ * @since       v1.5.0
+ * ---------------------------------------------------------------------------- */
+
+/**
+ * Current user controller.
+ *
+ * Handles current user settings related operations.
+ *
+ * @package Controllers
+ */
+class Current_user extends EA_Controller {
+    /**
+     * @var array
+     */
+    protected array $permissions;
+
+    /**
+     * Current_user constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('appointments_model');
+        $this->load->model('customers_model');
+        $this->load->model('services_model');
+        $this->load->model('providers_model');
+        $this->load->model('roles_model');
+        $this->load->model('settings_model');
+
+        $this->load->library('accounts');
+        $this->load->library('google_sync');
+        $this->load->library('notifications');
+        $this->load->library('synchronization');
+        $this->load->library('timezones');
+
+        $role_slug = session('role_slug');
+
+        if ($role_slug)
+        {
+            $this->permissions = $this->roles_model->get_permissions_by_slug($role_slug);
+        }
+    }
+
+    /**
+     * Render the settings page.
+     */
+    public function index()
+    {
+        session(['dest_url' => site_url('services')]);
+
+        if (cannot('view', 'services'))
+        {
+            show_error('Forbidden', 403);
+        }
+
+        $user_id = session('user_id');
+
+        $role_slug = session('role_slug');
+
+        $this->load->view('pages/settings/current_user/current_user_page', [
+            'page_title' => lang('settings'),
+            'active_menu' => PRIV_SYSTEM_SETTINGS,
+            'user_display_name' => $this->accounts->get_user_display_name($user_id),
+            'timezones' => $this->timezones->to_array(),
+            'privileges' => $this->roles_model->get_permissions_by_slug($role_slug),
+            'user_settings' => $this->users_model->find($user_id),
+        ]);
+    }
+
+    /**
+     * Save general settings.
+     */
+    public function save()
+    {
+        try
+        {
+            if ($this->permissions[PRIV_USER_SETTINGS]['edit'] == FALSE)
+            {
+                throw new Exception('You do not have the required permissions for this task.');
+            }
+
+            $settings = json_decode(request('settings'), TRUE);
+
+            $this->users_model->save($settings);
+
+            session([
+                'user_email' => $settings['email'],
+                'username' => $settings['settings']['username'],
+                'timezone' => $settings['timezone'],
+            ]);
+
+            response();
+        }
+        catch (Throwable $e)
+        {
+            json_exception($e);
+        }
+    }
+}
