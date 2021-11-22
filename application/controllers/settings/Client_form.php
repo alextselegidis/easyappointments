fc48db0ea499d5c64f1870c24f22cef088018b58
alextselegidis <alextselegidis@gmail.com>
2021-11-22 07:02:54 +0100

    Ported the client form settings functionality to a new standalone page

---
+++ application/controllers/settings/Client_form.php
@@ -0,0 +1,133 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
+ * @license     https://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        https://easyappointments.org
+ * @since       v1.5.0
+ * ---------------------------------------------------------------------------- */
+
+/**
+ * Client form controller.
+ *
+ * Handles client form settings related operations.
+ *
+ * @package Controllers
+ */
+class Client_form extends EA_Controller {
+    /**
+     * @var array
+     */
+    protected array $permissions;
+
+    /**
+     * Calendar constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('appointments_model');
+        $this->load->model('customers_model');
+        $this->load->model('services_model');
+        $this->load->model('providers_model');
+        $this->load->model('roles_model');
+        $this->load->model('settings_model');
+
+        $this->load->library('accounts');
+        $this->load->library('google_sync');
+        $this->load->library('notifications');
+        $this->load->library('synchronization');
+        $this->load->library('timezones');
+
+        $role_slug = session('role_slug');
+
+        if ($role_slug)
+        {
+            $this->permissions = $this->roles_model->get_permissions_by_slug($role_slug);
+        }
+    }
+
+    /**
+     * Render the settings page.
+     */
+    public function index()
+    {
+        session(['dest_url' => site_url('services')]);
+
+        if (cannot('view', 'services'))
+        {
+            show_error('Forbidden', 403);
+        }
+
+        $user_id = session('user_id');
+
+        $role_slug = session('role_slug');
+
+        $this->load->view('pages/settings/client_form/client_form_page', [
+            'page_title' => lang('settings'),
+            'active_menu' => PRIV_SYSTEM_SETTINGS,
+            'user_display_name' => $this->accounts->get_user_display_name($user_id),
+            'timezones' => $this->timezones->to_array(),
+            'privileges' => $this->roles_model->get_permissions_by_slug($role_slug),
+            'system_settings' => $this->settings_model->get(),
+        ]);
+    }
+
+    /**
+     * Save general settings.
+     */
+    public function save()
+    {
+        try
+        {
+            if ($this->permissions[PRIV_SYSTEM_SETTINGS]['edit'] == FALSE)
+            {
+                throw new Exception('You do not have the required permissions for this task.');
+            }
+
+            $settings = json_decode(request('settings', FALSE), TRUE);
+
+            // Check if phone number settings are valid.
+
+            $phone_number_required = setting('phone_number_required');
+
+            $phone_number_shown = FALSE;
+
+            foreach ($settings as $setting)
+            {
+                if ($setting['name'] === 'show_phone_number')
+                {
+                    $phone_number_shown = $setting['value'];
+                }
+            }
+
+            if ($phone_number_required && ! $phone_number_shown)
+            {
+                throw new RuntimeException('You cannot hide the phone number in the booking form while it\'s also required!');
+            }
+
+            foreach ($settings as $setting)
+            {
+                $existing_setting = $this->settings_model->query()->where('name', $setting['name'])->get()->row_array();
+
+                if ( ! empty($existing_setting))
+                {
+                    $setting['id'] = $existing_setting['id'];
+                }
+
+                $this->settings_model->save($setting);
+            }
+
+            response();
+        }
+        catch (Throwable $e)
+        {
+            json_exception($e);
+        }
+    }
+}
---
+++ assets/js/backend_settings_client_form_helper.js
@@ -0,0 +1,162 @@
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+(function () {
+    'use strict';
+
+    /**
+     * "System Settings" Tab Helper Class
+     *
+     * @class SystemSettingsClientFormHelper
+     */
+    var SystemSettingsClientFormHelper = function () {};
+
+    /**
+     * Save the system settings.
+     *
+     * This method is run after changes are detected on the tab input fields.
+     *
+     * @param {Array} settings Contains the system settings data.
+     */
+    SystemSettingsClientFormHelper.prototype.save = function (settings) {
+        if (!this.validate()) {
+            return; // Validation failed, do not proceed.
+        }
+
+        var url = GlobalVariables.baseUrl + '/index.php/settings/client_form/save';
+
+        var data = {
+            csrfToken: GlobalVariables.csrfToken,
+            settings: JSON.stringify(settings),
+            type: BackendSettingsClientForm.SETTINGS_SYSTEM
+        };
+
+        $.post(url, data).done(function () {
+            Backend.displayNotification(EALang.settings_saved);
+        });
+    };
+
+    /**
+     * Get the state of a visible/hidden toggle button
+     *
+     * This method uses the DOM elements of the backend/settings page, so it can't be used in another page.
+     *
+     * @argument the element jquery of a button object that is a visible/hidden toggle.
+     *
+     * @return '0' when the button shows 'invisible' and '1' when the button shows 'visible'. Will always return '0' on an error.
+     */
+    function getToggleButtonState($element) {
+        var visiblePartArray = $element.find('.hide-toggle-visible');
+        var invisiblePartArray = $element.find('.hide-toggle-hidden');
+        if (!(visiblePartArray.length === 0 || invisiblePartArray.length === 0)) {
+            if (visiblePartArray.hasClass('hidden')) {
+                //our button is currently invisible
+                return '0'; //invisible
+            } else {
+                //our button is currently visible
+                return '1'; //visible
+            }
+        } else {
+            return '0'; //invisible
+        }
+    }
+
+    /**
+     * Prepare the system settings array.
+     *
+     * This method uses the DOM elements of the backend/settings page, so it can't be used in another page.
+     *
+     * @return {Array} Returns the system settings array.
+     */
+    SystemSettingsClientFormHelper.prototype.get = function () {
+        var settings = [];
+
+        settings.push({
+            name: 'customer_notifications',
+            value: $('#customer-notifications').prop('checked') ? '1' : '0'
+        });
+
+        settings.push({
+            name: 'require_captcha',
+            value: $('#require-captcha').prop('checked') ? '1' : '0'
+        });
+
+        settings.push({
+            name: 'require_phone_number',
+            value: $('#require-phone-number').prop('checked') ? '1' : '0'
+        });
+
+        settings.push({
+            name: 'display_any_provider',
+            value: $('#display-any-provider').prop('checked') ? '1' : '0'
+        });
+
+        settings.push({
+            name: 'show_phone_number',
+            value: getToggleButtonState($('#show-phone-number'))
+        });
+
+        settings.push({
+            name: 'show_address',
+            value: getToggleButtonState($('#show-address'))
+        });
+
+        settings.push({
+            name: 'show_city',
+            value: getToggleButtonState($('#show-city'))
+        });
+
+        settings.push({
+            name: 'show_zip_code',
+            value: getToggleButtonState($('#show-zip-code'))
+        });
+
+        settings.push({
+            name: 'show_notes',
+            value: getToggleButtonState($('#show-notes'))
+        });
+
+        return settings;
+    };
+
+    /**
+     * Validate the settings data.
+     *
+     * If the validation fails then display a message to the user.
+     *
+     * @return {Boolean} Returns the validation result.
+     */
+    SystemSettingsClientFormHelper.prototype.validate = function () {
+        $('#client-form .has-error').removeClass('has-error');
+
+        try {
+            // Validate required fields.
+            var missingRequired = false;
+            $('#client-form .required').each(function (index, requiredField) {
+                if (!$(requiredField).val()) {
+                    $(requiredField).closest('.form-group').addClass('has-error');
+                    missingRequired = true;
+                }
+            });
+
+            if (missingRequired) {
+                throw new Error(EALang.fields_are_required);
+            }
+
+            return true;
+        } catch (error) {
+            Backend.displayNotification(error.message);
+            return false;
+        }
+    };
+
+    window.SystemSettingsClientFormHelper = SystemSettingsClientFormHelper;
+})();
