e0344049371c31bd889cc423e87c51d475f3f8b2
Alex Tselegidis <alextselegidis@gmail.com>
2021-11-29 09:01:02 +0100

    Added main app JS file

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.5.0
+ * ---------------------------------------------------------------------------- */
+
+window.App = (function () {
+    return {
+        Components: {},
+        Http: {},
+        Lang: {},
+        Layouts: {},
+        Pages: {},
+        Utils: {}
+    };
+})();
