49ec1d1caefbe521652a94b2ecccccee98cd6aac
Alex Tselegidis <alextselegidis@gmail.com>
2021-12-10 22:06:59 +0100

    Created new JS http client files

---
+++ assets/js/http/appointments_http_client.js
@@ -0,0 +1,14 @@
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.5.0
+ * ---------------------------------------------------------------------------- */
+
+window.App.Http.Appointments = (function () {
+    return {};
+})();
