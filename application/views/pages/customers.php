fe06d8382e574e00d3744a31002b60fcc8701c19
Alex Tselegidis <alextselegidis@gmail.com>
2022-01-07 08:42:28 +0100

    Add missing scripts to customers template

--- application/views/pages/customers.php
+++ application/views/pages/customers.php
@@ -209,9 +209,10 @@
 
 <?php section('scripts') ?>
 
-<script src="<?= asset_url('assets/js/utils/url.js') ?>"></script>
-<script src="<?= asset_url('assets/js/utils/message.js') ?>"></script>
 <script src="<?= asset_url('assets/js/utils/date.js') ?>"></script>
+<script src="<?= asset_url('assets/js/utils/message.js') ?>"></script>
+<script src="<?= asset_url('assets/js/utils/validation.js') ?>"></script>
+<script src="<?= asset_url('assets/js/utils/url.js') ?>"></script>
 <script src="<?= asset_url('assets/js/http/customers_http_client.js') ?>"></script>
 <script src="<?= asset_url('assets/js/pages/customers.js') ?>"></script>
 
