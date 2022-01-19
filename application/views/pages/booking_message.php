88b8742b10e39806e9a21c94b7ecf11eaa1e7f89
Alex Tselegidis <alextselegidis@gmail.com>
2022-01-19 10:25:04 +0100

    Move the analytics script to the booking message page

--- application/views/pages/booking_message.php
+++ application/views/pages/booking_message.php
@@ -3,14 +3,21 @@
 <?php section('content') ?>
 
 <div>
-    <img id="message-icon" src="<?= $message_icon ?>" alt="warning">
+    <img id="message-icon" src="<?= vars('message_icon') ?>" alt="warning">
 </div>
 
 <div>
-    <h3><?= $message_title ?></h3>
+    <h3><?= vars('message_title') ?></h3>
 
-    <p><?= $message_text ?></p>
+    <p><?= vars('message_text') ?></p>
 </div>
 
 <?php section('content') ?>
+
+<?php section('scripts') ?>
+
+<?php component('google_analytics_script', ['google_analytics_code' => vars('google_analytics_code')]) ?>
+<?php component('matomo_analytics_script', ['matomo_analytics_url' => vars('matomo_analytics_url')]) ?>
+
+<?php section('scripts') ?>
 
