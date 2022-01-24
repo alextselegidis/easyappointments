ad8308794c5d95c13f52727a755e360947920ca1
Alex Tselegidis <alextselegidis@gmail.com>
2022-01-24 13:42:13 +0100

    The login/logout/recovery page have a better structure.

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -4,38 +4,38 @@
 
 <h2><?= lang('backend_section') ?></h2>
 
-<p><?= lang('you_need_to_login') ?></p>
+<p>
+    <small>
+        <?= lang('you_need_to_login') ?>
+    </small>
+</p>
 
 <hr>
 <div class="alert d-none"></div>
 
 <form id="login-form">
-    <div class="mb-3">
-        <label for="username"><?= lang('username') ?></label>
-        <input type="text" id="username"
-               placeholder="<?= lang('enter_username_here') ?>"
-               class="form-control"/>
-    </div>
-    <div class="mb-3">
-        <label for="password"><?= lang('password') ?></label>
-        <input type="password" id="password"
-               placeholder="<?= lang('enter_password_here') ?>"
-               class="form-control"/>
+    <div class="mb-3 mt-5">
+        <label for="username" class="form-label">
+            <?= lang('username') ?>
+        </label>
+        <input type="text" id="username" placeholder="<?= lang('enter_username_here') ?>" class="form-control"/>
     </div>
 
-    <div class="mb-3">
+    <div class="mb-5">
+        <label for="password" class="form-label">
+            <?= lang('password') ?>
+        </label>
+        <input type="password" id="password" placeholder="<?= lang('enter_password_here') ?>" class="form-control"/>
+    </div>
+
+    <div class="d-flex justify-content-between align-items-center mb-5">
+        <a href="<?= site_url('recovery') ?>" class="forgot-password"><?= lang('forgot_your_password') ?></a>
+
         <button type="submit" id="login" class="btn btn-primary">
             <i class="fas fa-sign-in-alt me-2"></i>
             <?= lang('login') ?>
         </button>
     </div>
-
-    <a href="<?= site_url('recovery') ?>" class="forgot-password"><?= lang('forgot_your_password') ?></a>
-    |
-    <span id="select-language" class="badge bg-success">
-        <?= ucfirst(config('language')) ?>
-    </span>
-
 </form>
 <?php section('content') ?>
 
