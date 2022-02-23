c14a88ed939a490a03a24f800ddaa39fd7ca4fd0
Alex Tselegidis <alextselegidis@gmail.com>
2022-02-23 08:23:13 +0100

    Update the system path (#1207)

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = 'vendor/codeigniter/framework/system';
+	$system_path = 'system';
 
 /*
  *---------------------------------------------------------------
