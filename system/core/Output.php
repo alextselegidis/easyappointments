0100d0bd633910070b2a117ee565dea33e318a0e
Alex Tselegidis <alextselegidis@gmail.com>
2022-02-23 11:02:18 +0100

    PHP 8.1 support (#1209)

--- system/core/Output.php
+++ system/core/Output.php
@@ -454,7 +454,7 @@ class CI_Output {
 		if ($this->parse_exec_vars === TRUE)
 		{
 			$memory	= round(memory_get_usage() / 1024 / 1024, 2).'MB';
-			$output = str_replace(array('{elapsed_time}', '{memory_usage}'), array($elapsed, $memory), $output);
+			$output = str_replace(array('{elapsed_time}', '{memory_usage}'), array($elapsed, $memory), (string)$output);
 		}
 
 		// --------------------------------------------------------------------
