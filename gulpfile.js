bd9e9ff2e5432b171382fa600bc4f5035cff48fd
Alex Tselegidis <alextselegidis@gmail.com>
2022-02-25 17:54:06 +0100

    Add the patch.php file to the repository.

--- gulpfile.js
+++ gulpfile.js
@@ -58,6 +58,7 @@ gulp.task('package', (done) => {
     fs.copySync('storage/uploads/index.html', 'build/storage/uploads/index.html');
 
     fs.copySync('index.php', 'build/index.php');
+    fs.copySync('patch.php', 'build/patch.php');
     fs.copySync('composer.json', 'build/composer.json');
     fs.copySync('composer.lock', 'build/composer.lock');
     fs.copySync('config-sample.php', 'build/config-sample.php');
