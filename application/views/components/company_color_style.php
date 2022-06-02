9cab3427196930c78a7110b493256bd24aad7c49
Alex Tselegidis <alextselegidis@gmail.com>
2022-06-02 09:19:23 +0200

    Apply missing styles when overriding the default colors with the company colors

--- application/views/components/company_color_style.php
+++ application/views/components/company_color_style.php
@@ -4,90 +4,161 @@
  */
 ?>
 
-<style>
-    a {
-        color: <?= $company_color ?>;
-    }
+<?php if ($company_color !== DEFAULT_COMPANY_COLOR): ?>
+    <style>
+        /* Generic Overrides */
 
-    a:hover {
-        color: <?= $company_color ?>;
-        filter: brightness(120%);
-    }
+        a {
+            color: <?= $company_color ?>;
+        }
 
-    .btn-primary {
-        background-color: <?= $company_color ?>;
-        border-color: <?= $company_color ?>;
-    }
+        a:hover {
+            color: <?= $company_color ?>;
+            filter: brightness(120%);
+        }
 
-    .btn-primary:hover {
-        background-color: <?= $company_color ?>;
-        border-color: <?= $company_color ?>;
-        filter: brightness(120%);
-    }
-    
-    #book-appointment-wizard #header {
-        background: <?= $company_color ?>;
-    }
+        .btn-primary {
+            background-color: <?= $company_color ?>;
+            border-color: <?= $company_color ?>;
+        }
 
-    #book-appointment-wizard #company-name .display-selected-service, 
-    #book-appointment-wizard #company-name .display-selected-provider {
-        color: <?= $company_color ?>;
-        border-color: <?= $company_color ?> !important;
-        filter: brightness(35%);
-    }
+        .btn-primary:hover,
+        .btn-primary:active,
+        .btn-primary:focus {
+            background-color: <?= $company_color ?>;
+            border-color: <?= $company_color ?>;
+            filter: brightness(120%);
+            outline: none;
+        }
 
-    #book-appointment-wizard .book-step {
-        background: <?= $company_color ?>;
-        filter: brightness(75%);
-    }
+        .btn-primary:disabled, .btn-primary.disabled {
+            background-color: <?= $company_color ?>;
+            border-color: <?= $company_color ?>;
+            filter: brightness(70%);
+            opacity: .75;
+        }
 
-    #book-appointment-wizard .book-step strong {
-        color: <?= $company_color ?>;
-        filter: brightness(200%);
-    }
+        .dropdown-item.active,
+        .dropdown-item:active {
+            background-color: <?= $company_color ?> !important;
+        }
 
-    body .ui-widget.ui-widget-content {
-        border-color: <?= $company_color ?>;
-    }
+        .nav-pills .nav-link.active, .nav-pills .show > .nav-link {
+            background-color: <?= $company_color ?> !important;
+        }
 
-    body .ui-datepicker .ui-widget-header {
-        background-color: <?= $company_color ?>;
-    }
+        .nav .nav-link:not(.active) {
+            color: <?= $company_color ?> !important;
+        }
 
-    body .ui-datepicker th {
-        background-color: <?= $company_color ?>;
-    }
+        .form-control:focus {
+            border-color: <?= $company_color ?> !important;
+            filter: brightness(120%);
+            box-shadow: none;
+        }
 
-    body .ui-datepicker .ui-datepicker-next-hover,
-    body .ui-datepicker .ui-datepicker-prev-hover {
-        background: <?= $company_color ?>;
-        border-color: <?= $company_color ?>;
-        filter: brightness(140%);
-    }
+        .form-check-input:checked {
+            background-color: <?= $company_color ?> !important;
+            border-color: <?= $company_color ?> !important;
+        }
 
-    body .ui-datepicker td a, body .ui-datepicker td span {
-        color: <?= $company_color ?> !important;
-    }
+        body .ui-datepicker .ui-slider-handle {
+            border-color: <?= $company_color ?> !important;
+            background-color: <?= $company_color ?> !important;
+        }
 
-    html body .ui-datepicker td a.ui-state-active {
-        background: <?= $company_color ?> !important;
-    }
+        body .modal-header {
+            background:  <?= $company_color ?> !important;
+        }
 
-    body .ui-datepicker td a.ui-state-highlight {
-        background: <?= $company_color ?> !important;
-        filter: brightness(140%);
-    }
+        /* Booking Layout */
 
-    #book-appointment-wizard #available-hours .selected-hour {
-        background-color: <?= $company_color ?> ;
-        border-color: <?= $company_color ?> ;
-    }
-        
-    #frame-footer .backend-link {
-        background-color: <?= $company_color ?> !important;
-    }
+        #book-appointment-wizard #header {
+            background: <?= $company_color ?>;
+        }
 
-    #frame-footer .backend-link:hover {
-        color: #fff;
-    }
-</style>
+        #book-appointment-wizard #company-name .display-selected-service,
+        #book-appointment-wizard #company-name .display-selected-provider {
+            color: <?= $company_color ?>;
+            border-color: <?= $company_color ?> !important;
+            filter: brightness(35%);
+        }
+
+        #book-appointment-wizard .book-step {
+            background: <?= $company_color ?>;
+            filter: brightness(75%);
+        }
+
+        #book-appointment-wizard .book-step strong {
+            color: <?= $company_color ?>;
+            filter: brightness(200%);
+        }
+
+        body .ui-widget.ui-widget-content {
+            border-color: <?= $company_color ?>;
+        }
+
+        body .ui-datepicker .ui-widget-header {
+            background-color: <?= $company_color ?>;
+        }
+
+        body .ui-datepicker th {
+            background-color: <?= $company_color ?>;
+        }
+
+        body .ui-datepicker .ui-datepicker-next-hover,
+        body .ui-datepicker .ui-datepicker-prev-hover {
+            background: <?= $company_color ?>;
+            border-color: <?= $company_color ?>;
+            filter: brightness(140%);
+        }
+
+        body .ui-datepicker td a, body .ui-datepicker td span {
+            color: <?= $company_color ?> !important;
+        }
+
+        html body .ui-datepicker td a.ui-state-active {
+            background: <?= $company_color ?> !important;
+        }
+
+        body .ui-datepicker td a.ui-state-highlight {
+            background: <?= $company_color ?> !important;
+            filter: brightness(140%);
+        }
+
+        #book-appointment-wizard #available-hours .selected-hour {
+            background-color: <?= $company_color ?>;
+            border-color: <?= $company_color ?>;
+        }
+
+        #frame-footer .backend-link {
+            background-color: <?= $company_color ?> !important;
+        }
+
+        #frame-footer .backend-link:hover {
+            color: #fff;
+        }
+
+        /* Backend Layout */
+
+        #header {
+            background-color: <?= $company_color ?> !important;
+        }
+
+        #header #header-menu .nav-item:hover,
+        #header #header-menu .nav-item.active {
+            background: <?= $company_color ?> !important;
+            filter: brightness(120%);
+        }
+
+        #header #header-logo small {
+            color: <?= $company_color ?> !important;
+            filter: brightness(60%);
+        }
+
+        .backend-page .filter-records .results .entry.selected {
+            border-right-color: <?= $company_color ?> !important;
+        }
+
+    </style>
+<?php endif ?>
