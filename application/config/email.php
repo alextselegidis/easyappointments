ae83bd6d1deebee7872d7737ca454582a956eaea
Daneo Van Overloop <7900887+daneov@users.noreply.github.com>
2022-07-07 21:43:15 +0200

    Use environment variables to define email settings

--- application/config/email.php
+++ application/config/email.php
@@ -5,12 +5,12 @@
 // @link https://codeigniter.com/user_guide/libraries/email.html
 
 $config['useragent'] = 'Easy!Appointments';
-$config['protocol'] = 'mail'; // or 'smtp'
+$config['protocol'] = 'smtp'; // or 'smtp'
 $config['mailtype'] = 'html'; // or 'text'
 // $config['smtp_debug'] = '0'; // or '1'
-// $config['smtp_auth'] = TRUE; //or FALSE for anonymous relay.
-// $config['smtp_host'] = '';
-// $config['smtp_user'] = '';
-// $config['smtp_pass'] = '';
-// $config['smtp_crypto'] = 'ssl'; // or 'tls'
-// $config['smtp_port'] = 25;
+$config['smtp_auth'] = TRUE; //or FALSE for anonymous relay.
+$config['smtp_host'] = $_ENV['MAIL_SMTP_SERVER'] ?? '';
+$config['smtp_user'] = $_ENV['MAIL_SMTP_LOGIN'] ?? '';
+$config['smtp_pass'] = $_ENV['MAIL_SMTP_PASSWORD'] ?? '';
+$config['smtp_crypto'] = $_ENV['MAIL_SMTP_ENCRYPTION'] ?? 'tls'; // 'tls' or 'ssl'
+$config['smtp_port'] = $_ENV['MAIL_SMTP_PORT'] ?? 587;
