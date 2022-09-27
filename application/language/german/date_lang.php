1da9c2586d2236802efced72d209f8b1aad6e0e8
Lukas Herzog <git@lukasherzog.de>
2022-09-27 18:29:02 +0200

    update german language strings

--- application/language/german/date_lang.php
+++ application/language/german/date_lang.php
@@ -37,20 +37,20 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['date_year'] = 'Year';
-$lang['date_years'] = 'Years';
-$lang['date_month'] = 'Month';
-$lang['date_months'] = 'Months';
-$lang['date_week'] = 'Week';
-$lang['date_weeks'] = 'Weeks';
-$lang['date_day'] = 'Day';
-$lang['date_days'] = 'Days';
-$lang['date_hour'] = 'Hour';
-$lang['date_hours'] = 'Hours';
+$lang['date_year'] = 'Jahr';
+$lang['date_years'] = 'Jahre';
+$lang['date_month'] = 'Monat';
+$lang['date_months'] = 'Monate';
+$lang['date_week'] = 'Woche';
+$lang['date_weeks'] = 'Wochen';
+$lang['date_day'] = 'Tag';
+$lang['date_days'] = 'Tage';
+$lang['date_hour'] = 'Stunde';
+$lang['date_hours'] = 'Stunden';
 $lang['date_minute'] = 'Minute';
-$lang['date_minutes'] = 'Minutes';
-$lang['date_second'] = 'Second';
-$lang['date_seconds'] = 'Seconds';
+$lang['date_minutes'] = 'Minuten';
+$lang['date_second'] = 'Sekunde';
+$lang['date_seconds'] = 'Sekunden';
 
 $lang['UM12']	= '(UTC -12:00) Baker/Howland Island';
 $lang['UM11']	= '(UTC -11:00) Niue';
