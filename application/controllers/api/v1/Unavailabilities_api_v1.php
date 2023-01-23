04f480c4c6d081817a5820bcd1519a6e48f50e60
Alex Tselegidis <alextselegidis@gmail.com>
2023-01-23 07:47:47 +0100

    Remove duplicate command

--- application/controllers/api/v1/Unavailabilities_api_v1.php
+++ application/controllers/api/v1/Unavailabilities_api_v1.php
@@ -24,8 +24,6 @@ class Unavailabilities_api_v1 extends EA_Controller {
     {
         parent::__construct();
 
-        $this->load->model('unavailabilities_model');
-
         $this->load->library('api');
 
         $this->api->auth();
