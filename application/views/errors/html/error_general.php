ea3bdb17eefd469ff2b30944cb34041bd17243ad
Alex Tselegidis <alextselegidis@gmail.com>
2023-03-22 10:05:41 +0100

    Add the "user-scalable=no" meta value

--- application/views/errors/html/error_general.php
+++ application/views/errors/html/error_general.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <title>Error | Easy!Appointments</title>
     <style>
         #error-container {
