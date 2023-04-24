f7be8a1e1d8585ad6758ee28b548ab8d237f45ff
Alex Tselegidis <alextselegidis@gmail.com>
2023-04-24 10:02:44 +0200

    Center the email templates and set a min height for the content

--- application/views/emails/account_recovery_email.php
+++ application/views/emails/account_recovery_email.php
@@ -13,14 +13,14 @@
 </head>
 <body style="font: 13px arial, helvetica, tahoma;">
 
-<div class="email-container" style="width: 650px; border: 1px solid #eee;">
+<div class="email-container" style="width: 650px; border: 1px solid #eee; margin: 30px auto;">
     <div id="header" style="background-color: #429a82; height: 45px; padding: 10px 15px;">
         <strong id="logo" style="color: white; font-size: 20px; margin-top: 10px; display: inline-block">
             <?= e($settings['company_name']) ?>
         </strong>
     </div>
 
-    <div id="content" style="padding: 10px 15px;">
+    <div id="content" style="padding: 10px 15px; min-height: 400px">
         <h2>
             <?= $subject ?>
         </h2>
