1655b0df2d9ee666300ce742b5b51b59d148dca2
Alex Tselegidis <alextselegidis@gmail.com>
2023-05-03 07:21:20 +0200

    Correct the captcha builer php issues.

--- application/controllers/Captcha.php
+++ application/controllers/Captcha.php
@@ -36,14 +36,14 @@ class Captcha extends EA_Controller {
      */
     public function index()
     {
-        $builder = new CaptchaBuilder;
-        $builder->setDistortion(false);
-        $builder->setMaxBehindLines(1);
-        $builder->setMaxFrontLines(1);
-        $builder->setBackgroundColor(255,255,255);
-        $builder->build();
-        session(['captcha_phrase' => $builder->getPhrase()]);
+
+        $this->captcha_builder->setDistortion(false);
+        $this->captcha_builder->setMaxBehindLines(1);
+        $this->captcha_builder->setMaxFrontLines(1);
+        $this->captcha_builder->setBackgroundColor(255,255,255);
+        $this->captcha_builder->build();
+        session(['captcha_phrase' => $this->captcha_builder->getPhrase()]);
         header('Content-type: image/jpeg');
-        $builder->output();
+        $this->captcha_builder->output();
     }
 }
