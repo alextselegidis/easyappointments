121571f517702362a04c81c085da9105fdb164c6
Alex Tselegidis <alextselegidis@gmail.com>
2023-05-04 17:56:43 +0200

    Display the any-provider option only if there are more than one providers available for selection

--- assets/js/pages/booking.js
+++ assets/js/pages/booking.js
@@ -306,7 +306,7 @@ App.Pages.Booking = (function () {
             });
 
             // Add the "Any Provider" entry.
-            if ($selectProvider.find('option').length >= 1 && vars('display_any_provider') === '1') {
+            if ($selectProvider.find('option').length > 1 && vars('display_any_provider') === '1') {
                 $selectProvider.prepend(new Option(lang('any_provider'), 'any-provider', true, true));
             }
 
