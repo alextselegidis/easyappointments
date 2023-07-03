a225019f6f1495bda1277d5af2c414dd6fdf0918
Alex Tselegidis <alextselegidis@gmail.com>
2023-07-03 10:19:05 +0200

    Add the Google "notranslate" meta to all layouts (#1360)

--- application/views/layouts/account_layout.php
+++ application/views/layouts/account_layout.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="theme-color" content="#35A768">
-
+    <meta name="google" content="notranslate">
+    
     <?php slot('meta') ?>
 
     <title><?= vars('page_title') ?? lang('account') ?> | Easy!Appointments</title>
--- application/views/layouts/message_layout.php
+++ application/views/layouts/message_layout.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="theme-color" content="#35A768">
+    <meta name="google" content="notranslate">
     
     <?php slot('meta') ?>
     
