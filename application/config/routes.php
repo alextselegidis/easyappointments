007145e2132f869dec899350098eaeefc79125c0
Alex Tselegidis <alextselegidis@gmail.com>
2023-08-01 22:08:55 +0200

    Set up swagger-ui in the docker compose (port 8000)

--- application/config/routes.php
+++ application/config/routes.php
@@ -82,21 +82,21 @@ header('X-Frame-Options: SAMEORIGIN');
 |
 */
 
+header('Access-Control-Allow-Origin: *'); // NOTICE: Change this header to restrict CORS access.
+
+if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+{
+    // May also be using PUT, PATCH, HEAD etc
+    header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD');
+}
+
+if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+{
+    header('Access-Control-Allow-Headers: ' . $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
+}
+
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS')
 {
-    header('Access-Control-Allow-Origin: *');
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-    {
-        // May also be using PUT, PATCH, HEAD etc
-        header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    }
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-    {
-        header('Access-Control-Allow-Headers: ' . $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
-    }
-
     exit(0);
 }
 
