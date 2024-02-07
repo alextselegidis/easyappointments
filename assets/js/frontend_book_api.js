f40f44807b0f1680d90ddf78e2b28ca1543550f1
kjporter <kporter@kplink.net>
2024-02-07 18:36:42 -0500

    Update frontend_book_api.js

--- assets/js/frontend_book_api.js
+++ assets/js/frontend_book_api.js
@@ -273,6 +273,9 @@ window.FrontendBookApi = window.FrontendBookApi || {};
     };
 
     function applyUnavailableDates(unavailableDates, selectedDateString, setDate) {
+        // ZTL Modification - allows enforcement booking policy no more than 2 weeks in the future
+        const BOOKINGWINDOW = 14; // How many days in the future is booking open for students?
+        let bookingWindowTime = new Date.getTime() + (BOOKINGWINDOW * 24 * 60 * 60 * 1000);
         setDate = setDate || false;
 
         processingUnavailabilities = true;
@@ -284,7 +287,7 @@ window.FrontendBookApi = window.FrontendBookApi || {};
         if (setDate && !GlobalVariables.manageMode) {
             for (var i = 1; i <= numberOfDays; i++) {
                 var currentDate = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), i);
-                if (unavailableDates.indexOf(currentDate.toString('yyyy-MM-dd')) === -1) {
+                if ((unavailableDates.indexOf(currentDate.toString('yyyy-MM-dd')) === -1) && (currentDate.getTime() <= bookingWindowTime)) {
                     $('#select-date').datepicker('setDate', currentDate);
                     FrontendBookApi.getAvailableHours(currentDate.toString('yyyy-MM-dd'));
                     break;
