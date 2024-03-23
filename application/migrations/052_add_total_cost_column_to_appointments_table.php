6f47a1af5765948419426f4840e3ed073dbcddff
Steven Kissack <steven@osar.im>
2024-03-23 20:41:21 +0700

    migration fix: migrate existing appointments & drop currency in down

--- application/migrations/052_add_total_cost_column_to_appointments_table.php
+++ application/migrations/052_add_total_cost_column_to_appointments_table.php
@@ -23,7 +23,7 @@ class Migration_Add_total_cost_column_to_appointments_table extends EA_Migration
                 'total_cost' => [
                     'type' => 'DECIMAL',
                     'constraint' => '10,2',
-                    'null' => TRUE,
+                    'null' => true,
                     'after' => 'status',
                 ],
                 'currency' => [
@@ -34,6 +34,14 @@ class Migration_Add_total_cost_column_to_appointments_table extends EA_Migration
             ];
 
             $this->dbforge->add_column('appointments', $fields);
+
+            // Update existing records
+            $sql = "UPDATE `appointments`
+            JOIN `services` ON `appointments`.`id_services` = `services`.`id`
+            SET `appointments`.`total_cost` = `services`.`price`,
+                `appointments`.`currency` = `services`.`currency`";
+            $this->db->query($sql);
+
         }
     }
 
@@ -44,6 +52,7 @@ class Migration_Add_total_cost_column_to_appointments_table extends EA_Migration
     {
         if ($this->db->field_exists('total_cost', 'appointments')) {
             $this->dbforge->drop_column('appointments', 'total_cost');
+            $this->dbforge->drop_column('appointments', 'currency');
         }
     }
 }
