0706d3fdab68652cb9829a2937a668d214b4bc59
Alex Tselegidis <alextselegidis@gmail.com>
2024-05-11 16:12:14 +0200

    The webhook actions are optional

--- application/models/Webhooks_model.php
+++ application/models/Webhooks_model.php
@@ -72,7 +72,7 @@ class Webhooks_model extends EA_Model
      */
     public function validate(array $webhook): void
     {
-        if (empty($webhook['name']) || empty($webhook['url']) || empty($webhook['actions'])) {
+        if (empty($webhook['name']) || empty($webhook['url'])) {
             throw new InvalidArgumentException('Not all required fields are provided: ' . print_r($webhook, true));
         }
     }
