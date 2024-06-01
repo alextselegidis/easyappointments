f324244153c7cccd20f409e9a71a52fa9b5ee1b5
Saud <saud@saudsoft.com>
2024-06-01 20:59:54 +0300

    Edit Arabic language translations

--- application/language/arabic/number_lang.php
+++ application/language/arabic/number_lang.php
@@ -37,8 +37,8 @@
  */
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['terabyte_abbr'] = 'TB';
-$lang['gigabyte_abbr'] = 'GB';
-$lang['megabyte_abbr'] = 'MB';
-$lang['kilobyte_abbr'] = 'KB';
-$lang['bytes'] = 'Bytes';
+$lang['terabyte_abbr'] = 'تيرابايت';
+$lang['gigabyte_abbr'] = 'جيجابايت';
+$lang['megabyte_abbr'] = 'ميجابايت';
+$lang['kilobyte_abbr'] = 'كيلوبايت';
+$lang['bytes'] = 'بايت';
