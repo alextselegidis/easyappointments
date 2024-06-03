1b32f83818ae8f527d45db49e67dd197344ce614
Alex Tselegidis <alextselegidis@gmail.com>
2024-06-03 19:13:21 +0200

    Update default config-sample.php values

--- config-sample.php
+++ config-sample.php
@@ -24,30 +24,30 @@
  * If you are updating from version 1.0 you will have to create a new "config.php"
  * file because the old "configuration.php" is not used anymore.
  */
-class Config {
-
+class Config
+{
     // ------------------------------------------------------------------------
     // GENERAL SETTINGS
     // ------------------------------------------------------------------------
 
-    const BASE_URL      = 'http://url-to-easyappointments-directory';
-    const LANGUAGE      = 'english';
-    const DEBUG_MODE    = FALSE;
+    const BASE_URL = 'http://localhost';
+    const LANGUAGE = 'english';
+    const DEBUG_MODE = false;
 
     // ------------------------------------------------------------------------
     // DATABASE SETTINGS
     // ------------------------------------------------------------------------
 
-    const DB_HOST       = 'localhost';
-    const DB_NAME       = 'easyappointments';
-    const DB_USERNAME   = 'root';
-    const DB_PASSWORD   = 'root';
+    const DB_HOST = 'mysql';
+    const DB_NAME = 'easyappointments';
+    const DB_USERNAME = 'user';
+    const DB_PASSWORD = 'password';
 
     // ------------------------------------------------------------------------
     // GOOGLE CALENDAR SYNC
     // ------------------------------------------------------------------------
 
-    const GOOGLE_SYNC_FEATURE   = FALSE; // Enter TRUE or FALSE
-    const GOOGLE_CLIENT_ID      = '';
-    const GOOGLE_CLIENT_SECRET  = '';
+    const GOOGLE_SYNC_FEATURE = false; // Enter TRUE or FALSE
+    const GOOGLE_CLIENT_ID = '';
+    const GOOGLE_CLIENT_SECRET = '';
 }
