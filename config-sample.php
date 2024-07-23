a69a2f8b2d2125b556ff2fa21c26b58884bebd49
Marcel Link <marcel.link@t-online.de>
2024-07-23 13:55:13 +0200

    Add basic e-mail settings to config.php

--- config-sample.php
+++ config-sample.php
@@ -50,4 +50,37 @@ class Config
     const GOOGLE_SYNC_FEATURE = false; // Enter TRUE or FALSE
     const GOOGLE_CLIENT_ID = '';
     const GOOGLE_CLIENT_SECRET = '';
+
+    // ------------------------------------------------------------------------
+    // E-MAIL SETTINGS
+    // ------------------------------------------------------------------------
+
+    // Add custom values by settings them to the $config array in application/config/email.php.
+    // @link https://codeigniter.com/user_guide/libraries/email.html
+
+    const EMAIL_USERAGENT = 'Easy!Appointments';
+    const EMAIL_PROTOCOL = 'mail'; // or 'smtp'
+    const EMAIL_MAILTYPE = 'html';  // or 'text'
+    const EMAIL_SMTP_DEBUG = '0';  // or '1'
+    const EMAIL_SMTP_AUTH = TRUE; //or FALSE for anonymous relay.
+    const EMAIL_SMTP_HOST = '';
+    const EMAIL_SMTP_USER = '';
+    const EMAIL_SMTP_PASS = '';
+    const EMAIL_SMTP_CRYPTO = 'ssl';
+    const EMAIL_SMTP_PORT = 25;
+    const EMAIL_FROM_NAME = '';
+    const EMAIL_FROM_ADDRESS = '';
+    const EMAIL_REPLY_TO = '';
+
+
+    /* Example Exchange OnPremise 2019
+    const EMAIL_PROTOCOL = 'smtp'; // or 'smtp'
+    const EMAIL_SMTP_AUTH = TRUE; //or FALSE for anonymous relay.
+    const EMAIL_SMTP_HOST = ''; // hostname
+    const EMAIL_SMTP_USER = ''; // e-mail
+    const EMAIL_SMTP_PASS = ''; // password
+    const EMAIL_SMTP_CRYPTO = 'tls'; // STARTTLS necessary
+    const EMAIL_SMTP_PORT = 587;
+    const EMAIL_FROM_ADDRESS = ''; // e-mail
+    */
 }
