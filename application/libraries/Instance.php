fc3d70aeacc6c51981e236ed22bac3085d2caaec
Alex Tselegidis <alextselegidis@gmail.com>
2025-01-20 17:27:46 +0100

    Nullable param

--- application/libraries/Instance.php
+++ application/libraries/Instance.php
@@ -162,7 +162,7 @@ class Instance
      *
      * @throws Exception
      */
-    public function backup(string $path = null): void
+    public function backup(?string $path): void
     {
         $path = $path ?? APPPATH . '/../storage/backups';
 
