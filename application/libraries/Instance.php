4c113e9c3cfcc91b8e8efe124dbedfec6cd8e0c9
Alex Tselegidis <alextselegidis@gmail.com>
2025-04-07 17:08:10 +0200

    Default value for the $path param

--- application/libraries/Instance.php
+++ application/libraries/Instance.php
@@ -162,7 +162,7 @@ class Instance
      *
      * @throws Exception
      */
-    public function backup(?string $path): void
+    public function backup(?string $path = null): void
     {
         $path = $path ?? APPPATH . '/../storage/backups';
 
