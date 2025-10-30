2d3802d5ccfb999247f4499de1182c00eb069bd0
Alex Tselegidis <alextselegidis@gmail.com>
2025-10-30 16:48:05 +0100

    Remove the phone field from the installation (#1709)

--- application/views/pages/installation.php
+++ application/views/pages/installation.php
@@ -70,14 +70,6 @@
             </div>
 
             <div class="mb-3">
-                <label class="form-label" for="phone-number">
-                    <?= lang('phone_number') ?>
-                    <span class="text-danger">*</span>
-                </label>
-                <input id="phone-number" class="form-control required" maxlength="128">
-            </div>
-
-            <div class="mb-3">
                 <label class="form-label" for="username">
                     <?= lang('username') ?>
                     <span class="text-danger">*</span>
