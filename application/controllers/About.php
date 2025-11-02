23df59b82893d3cb3122dad2930a666cef57094c
Alex Tselegidis <alextselegidis@gmail.com>
2025-11-02 16:04:46 +0100

    Correct page title

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -68,7 +68,7 @@ class About extends EA_Controller
         ]);
 
         html_vars([
-            'page_title' => lang('settings'),
+            'page_title' => lang('about'),
             'active_menu' => PRIV_SYSTEM_SETTINGS,
             'user_display_name' => $this->accounts->get_user_display_name($user_id),
             'privileges' => $this->roles_model->get_permissions_by_slug($role_slug),
