93a8153766df010d86b6916c3a1f41bd9849d090
C. Neubauer | CN-Tools <cn-tools@gmx.at>
2026-08-02 15:55:54 +0200

    fix: update theme color meta tag to use company color setting

--- application/views/layouts/backend_layout.php
+++ application/views/layouts/backend_layout.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta name="theme-color" content="#35A768">
+    <meta name="theme-color" content="<?= setting('company_color', '#35A768') ?>">
     <meta name="google" content="notranslate">
 
     <?php slot('meta'); ?>
