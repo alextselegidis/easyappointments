fb8f7164d0173563b387b7ce92da7bde9ff1ae63
Alex Tselegidis <alextselegidis@gmail.com>
2026-08-19 18:00:07 +0200

    Release v1.6.1-alpha.1

--- application/config/app.php
+++ application/config/app.php
@@ -9,10 +9,10 @@
 |
 */
 
-$config['version'] = '1.6.0'; // This must be changed manually.
+$config['version'] = '1.6.1-alpha.1'; // This must be changed manually.
 
 $config['url'] = Config::BASE_URL;
 
 $config['debug'] = Config::DEBUG_MODE;
 
-$config['cache_busting_token'] = 'TSJ83';
+$config['cache_busting_token'] = 'TSJ83-985';
